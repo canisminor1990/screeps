98e0c9c568211edca4ac9dfd8c45f66bd3390eff
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 18:44:33 +0800

    'updata'

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const BabelConfig = {
     ],
     plugins: [
         'babel-plugin-transform-es2017-object-entries',
-        // 'babel-plugin-transform-runtime',
+        'babel-plugin-transform-runtime',
         "babel-plugin-dev-expression", // 替换 __DEV__  invariant warning
         'babel-plugin-add-module-exports',  // 解决es6 export default问题
         'babel-plugin-transform-decorators-legacy', // 支持装饰器
