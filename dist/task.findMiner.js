4e8cf35531b3817a3d20ba154a307712e64d6a97
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 23:52:22 +0800

    'updata'

--- dist/task.findMiner.js
+++ dist/task.findMiner.js
@@ -5,19 +5,16 @@ Object.defineProperty(exports, "__esModule", {
 });
 var taskFindMiner = function taskFindMiner(creep) {
     for (var name in Game.creeps) {
-        var miner = Game.creeps[name];
-        if (miner.memory.role === 'miner' && creep.memory.source === creep.memory.source) {
-            return miner;
+        var _miner = Game.creeps[name];
+        if (_miner.memory.role === 'miner' && creep.memory.source === creep.memory.source) {
+            return _miner;
             break;
         }
+    }
+    if (miner.carry.energy < miner.carryCapacity) {
+        return creep.harvest(rawSource) === ERR_NOT_IN_RANGE ? creep.moveTo(rawSource, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
+    } else {
         return creep.moveTo(miner, { visualizePathStyle: { stroke: '#ffaa00' } });
-        var rawSource = creep.room.find(FIND_SOURCES)[creep.memory.source];
-
-        if (miner.carry.energy < miner.carryCapacity) {
-            return creep.harvest(rawSource) === ERR_NOT_IN_RANGE ? creep.moveTo(rawSource, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
-        } else {
-            return creep.moveTo(miner, { visualizePathStyle: { stroke: '#ffaa00' } });
-        }
     }
 };
 
--- src/task.findMiner.js
+++ src/task.findMiner.js
@@ -1,18 +1,17 @@
 const taskFindMiner = (creep) => {
     for (let name in Game.creeps) {
-        const miner = Game.creeps[name];
+        let miner = Game.creeps[name];
         if (miner.memory.role === 'miner' && creep.memory.source === creep.memory.source) {
             return miner;
             break;
         }
-        return  creep.moveTo(miner, {visualizePathStyle: {stroke: '#ffaa00'}});
-        const rawSource = creep.room.find(FIND_SOURCES)[creep.memory.source]
 
-        if (miner.carry.energy < miner.carryCapacity) {
-            return (creep.harvest(rawSource) === ERR_NOT_IN_RANGE) ? creep.moveTo(rawSource, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
-        } else {
-            return  creep.moveTo(miner, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
+
+    }
+    if (miner.carry.energy < miner.carryCapacity) {
+        return (creep.harvest(rawSource) === ERR_NOT_IN_RANGE) ? creep.moveTo(rawSource, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
+    } else {
+        return  creep.moveTo(miner, {visualizePathStyle: {stroke: '#ffaa00'}});
     }
 }
 
