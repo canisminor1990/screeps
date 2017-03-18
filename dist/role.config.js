04eef5c7c33da2713ec890ebaa28e62454d09cff
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 22:15:05 +0800

    'updata'

--- dist/role.config.js
+++ dist/role.config.js
@@ -8,7 +8,7 @@ var roleConfig = {
         harvester: [0, 8],
         upgrader: [1],
         builder: [1],
-        miner: [0]
+        miner: [0, 1]
     },
     body: {
         miner: buildBody({ move: 1, work: 2, carry: 1 }),
