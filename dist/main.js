70d35d27cca6721be8331979e995b55eb83327e4
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 21:30:37 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -22,23 +22,22 @@ module.exports = {
             Game.spawns['Spawn1'].room.visual.text('[Spawn]' + spawningCreep.memory.role, Game.spawns['Spawn1'].pos.x + 1, Game.spawns['Spawn1'].pos.y, { align: 'left', opacity: 0.8 });
         }
 
-        var targets = creep.room.find(FIND_STRUCTURES, {
-            filter: function filter(structure) {
-                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-            }
-        });
-
         for (var _name in Game.creeps) {
-            var _creep = Game.creeps[_name];
-            switch (_creep.memory.role) {
+            var creep = Game.creeps[_name];
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: function filter(structure) {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                }
+            });
+            switch (creep.memory.role) {
                 case 'harvester':
-                    targets.length > 0 ? _role.roleHarvester.run(_creep, targets[0]) : _role.roleBuilder.run(_creep);
+                    targets.length > 0 ? _role.roleHarvester.run(creep, targets[0]) : _role.roleBuilder.run(creep);
                     break;
                 case 'upgrader':
-                    targets.length > 0 ? _role.roleHarvester.run(_creep, targets[0]) : _role.roleUpgrader.run(_creep);
+                    targets.length > 0 ? _role.roleHarvester.run(creep, targets[0]) : _role.roleUpgrader.run(creep);
                     break;
                 case 'builder':
-                    targets.length > 0 ? _role.roleHarvester.run(_creep, targets[0]) : _role.roleBuilder.run(_creep);
+                    targets.length > 0 ? _role.roleHarvester.run(creep, targets[0]) : _role.roleBuilder.run(creep);
                     break;
             }
         }
