4aca65f83d51b5392a3c666f8dbbc9e97365b5a4
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 14:53:46 +0800

    sync

--- dist/main.js
+++ dist/main.js
@@ -13,36 +13,40 @@ function taskBuild(x, y, type) {
 	}
 }
 
-module.exports.loop = function () {
+module.exports = {
+	taskBuild: taskBuild,
+	loop: function loop() {
 
-	for (var name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			delete Memory.creeps[name];
-			console.log('Clearing non-existing creep memory:', name);
+		for (var name in Memory.creeps) {
+			if (!Game.creeps[name]) {
+				delete Memory.creeps[name];
+				console.log('Clearing non-existing creep memory:', name);
+			}
+		}
+
+		(0, _task.taskSpawn)('harvester', 2);
+		(0, _task.taskSpawn)('upgrader', 1);
+		(0, _task.taskSpawn)('builder', 1);
+
+		if (Game.spawns['Spawn1'].spawning) {
+			var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+			Game.spawns['Spawn1'].room.visual.text('[Spawn]' + spawningCreep.memory.role, Game.spawns['Spawn1'].pos.x + 1, Game.spawns['Spawn1'].pos.y, { align: 'left', opacity: 0.8 });
+		}
+
+		for (var _name in Game.creeps) {
+			var creep = Game.creeps[_name];
+			switch (creep.memory.role) {
+				case 'harvester':
+					Game.spawns['Spawn1'].energy < 300 ? _role.roleHarvester.run(creep) : _role.roleUpgrader.run(creep);
+					break;
+				case 'upgrader':
+					_role.roleUpgrader.run(creep);
+					break;
+				case 'builder':
+					_role.roleBuilder.run(creep);
+					break;
+			}
 		}
 	}
 
-	(0, _task.taskSpawn)('harvester', 2);
-	(0, _task.taskSpawn)('upgrader', 1);
-	(0, _task.taskSpawn)('builder', 1);
-
-	if (Game.spawns['Spawn1'].spawning) {
-		var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-		Game.spawns['Spawn1'].room.visual.text('[Spawn]' + spawningCreep.memory.role, Game.spawns['Spawn1'].pos.x + 1, Game.spawns['Spawn1'].pos.y, { align: 'left', opacity: 0.8 });
-	}
-
-	for (var _name in Game.creeps) {
-		var creep = Game.creeps[_name];
-		switch (creep.memory.role) {
-			case 'harvester':
-				Game.spawns['Spawn1'].energy < 300 ? _role.roleHarvester.run(creep) : _role.roleUpgrader.run(creep);
-				break;
-			case 'upgrader':
-				_role.roleUpgrader.run(creep);
-				break;
-			case 'builder':
-				_role.roleBuilder.run(creep);
-				break;
-		}
-	}
 };
--- src/main.js
+++ src/main.js
@@ -31,41 +31,45 @@ function taskBuild(x, y, type) {
 	}
 }
 
-module.exports.loop = () => {
+module.exports = {
+	taskBuild: taskBuild,
+	loop: () => {
 
-	for (let name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			delete Memory.creeps[name];
-			console.log('Clearing non-existing creep memory:', name);
+		for (let name in Memory.creeps) {
+			if (!Game.creeps[name]) {
+				delete Memory.creeps[name];
+				console.log('Clearing non-existing creep memory:', name);
+			}
+		}
+
+		taskSpawn('harvester', 2)
+		taskSpawn('upgrader', 1)
+		taskSpawn('builder', 1)
+
+		if (Game.spawns['Spawn1'].spawning) {
+			const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+			Game.spawns['Spawn1'].room.visual.text(
+					'[Spawn]' + spawningCreep.memory.role,
+					Game.spawns['Spawn1'].pos.x + 1,
+					Game.spawns['Spawn1'].pos.y,
+					{align: 'left', opacity: 0.8});
+		}
+
+		for (let name in Game.creeps) {
+			const creep = Game.creeps[name];
+			switch (creep.memory.role) {
+				case 'harvester':
+					(Game.spawns['Spawn1'].energy < 300) ?
+							roleHarvester.run(creep) : roleUpgrader.run(creep);
+					break;
+				case 'upgrader':
+					roleUpgrader.run(creep);
+					break;
+				case 'builder':
+					roleBuilder.run(creep);
+					break;
+			}
 		}
 	}
 
-	taskSpawn('harvester', 2)
-	taskSpawn('upgrader', 1)
-	taskSpawn('builder', 1)
-
-	if (Game.spawns['Spawn1'].spawning) {
-		const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-		Game.spawns['Spawn1'].room.visual.text(
-				'[Spawn]' + spawningCreep.memory.role,
-				Game.spawns['Spawn1'].pos.x + 1,
-				Game.spawns['Spawn1'].pos.y,
-				{align: 'left', opacity: 0.8});
-	}
-
-	for (let name in Game.creeps) {
-		const creep = Game.creeps[name];
-		switch (creep.memory.role) {
-			case 'harvester':
-				(Game.spawns['Spawn1'].energy < 300) ?
-						roleHarvester.run(creep) : roleUpgrader.run(creep);
-				break;
-			case 'upgrader':
-				roleUpgrader.run(creep);
-				break;
-			case 'builder':
-				roleBuilder.run(creep);
-				break;
-		}
-	}
 }
