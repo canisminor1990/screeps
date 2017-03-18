0c5a5dfd34cc9172311fa444e3498bfecbe95936
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 09:55:10 +0800

    sync

--- dist/role.upgrader.js
+++ dist/role.upgrader.js
@@ -4,16 +4,23 @@ exports.__esModule = true;
 var roleUpgrader = {
 	run: function run(creep) {
 
-		if (creep.carry.energy < 50) {
-			var sources = creep.room.find(FIND_SOURCES);
-			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-				creep.say('harvest');
-				creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+		if (creep.memory.upgrading && creep.carry.energy == 0) {
+			creep.memory.upgrading = false;
+			creep.say('harvest');
+		}
+		if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+			creep.memory.upgrading = true;
+			creep.say('upgrade');
+		}
+
+		if (creep.memory.upgrading) {
+			if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
 			}
 		} else {
-			if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-				creep.say('upgrade');
-				creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+			var sources = creep.room.find(FIND_SOURCES);
+			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
 			}
 		}
 	}
