d0ad5b54368c6e2b66a076d3a707bab41ea60f68
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 00:05:56 +0800

    'updata'

--- dist/task.findMiner.js
+++ dist/task.findMiner.js
@@ -15,7 +15,7 @@ var taskFindMiner = function taskFindMiner(creep) {
         }
     }
 
-    if (minerTarget.carry.energy < minerTarget.carryCapacity) {
+    if (minerTarget && minerTarget.carry.energy < minerTarget.carryCapacity) {
         return creep.harvest(rawSource) === ERR_NOT_IN_RANGE ? creep.moveTo(rawSource, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
     } else {
         return creep.moveTo(minerTarget, { visualizePathStyle: { stroke: '#ffaa00' } });
