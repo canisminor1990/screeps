5a85d4cff8f53351416d35d64f05becda6baea37
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 01:07:01 +0800

    updata

--- dist/task.spawn.js
+++ dist/task.spawn.js
@@ -6,17 +6,14 @@ Object.defineProperty(exports, "__esModule", {
 var taskSpawn = function taskSpawn(number, body) {
 	var _loop = function _loop(key) {
 		var roleSpawn = key;
-		var maxNum = 0;
 		for (var i = 0; i < number[key].length; i++) {
-			maxNum = maxNum + number[key][i];
-		}
-		for (var _i = 0; _i < number[key].length; _i++) {
+			var maxNum = number[key][i];
 			var roleNumber = _.filter(Game.creeps, function (creep) {
 				return creep.memory.role == roleSpawn;
 			}).length;
-			if (number[key][_i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key]) === OK) {
-				Game.spawns['Spawn1'].createCreep(body[key], '[' + roleSpawn + ']' + getNowFormatDate(), { role: roleSpawn, source: _i });
-				console.log('Spawn:', roleSpawn, _i);
+			if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key]) === OK) {
+				Game.spawns['Spawn1'].createCreep(body[key], '[' + roleSpawn + ']' + getNowFormatDate(), { role: roleSpawn, source: i });
+				console.log('Spawn:', roleSpawn, i);
 			}
 		}
 	};
--- src/task.spawn.js
+++ src/task.spawn.js
@@ -2,11 +2,8 @@ const taskSpawn = (number, body) => {
 
 	for (let key in number) {
 		const roleSpawn = key;
-		let maxNum = 0
 		for (let i = 0; i < number[key].length; i++) {
-			maxNum = maxNum + number[key][i]
-		}
-		for (let i = 0; i < number[key].length; i++) {
+			const maxNum =  number[key][i]
 			const roleNumber = _.filter(Game.creeps, (creep) => creep.memory.role == roleSpawn).length;
 			if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key]) === OK) {
 				Game.spawns['Spawn1'].createCreep(
