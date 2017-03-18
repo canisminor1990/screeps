f6925a883bb9cb073796f4b5f788b309e12dac5b
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 22:32:47 +0800

    'updata'

--- dist/role.miner.js
+++ dist/role.miner.js
@@ -9,7 +9,7 @@ var roleMiner = {
             var source = creep.room.find(FIND_SOURCES)[creep.memory.source];
             creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
         } else {
-            var targets = creep.pos.findInRange(FIND_MY_CREEPS, 3);
+            var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
             console.log(creep.transfer(targets[0], RESOURCE_ENERGY, creep.carry.energy));
         }
     }
--- src/role.miner.js
+++ src/role.miner.js
@@ -5,7 +5,7 @@ const roleMiner = {
             (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
         }
         else {
-            const targets = creep.pos.findInRange(FIND_MY_CREEPS, 3);
+            const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
             console.log(creep.transfer(targets[0], RESOURCE_ENERGY, creep.carry.energy))
         }
     }
