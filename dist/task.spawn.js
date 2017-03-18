bd0ae7bfa5a4c8fd2d2dec00d3e90bf74878f3de
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 21:49:53 +0800

    'updata'

--- dist/task.spawn.js
+++ dist/task.spawn.js
@@ -6,14 +6,16 @@ Object.defineProperty(exports, "__esModule", {
 var taskSpawn = function taskSpawn(number, body) {
     var _loop = function _loop(key) {
 
-        var roleSpawn = key,
-            maxNum = number[key],
-            roleNumber = _.filter(Game.creeps, function (creep) {
-            return creep.memory.role == roleSpawn;
-        }).length;
-        if (roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key])) {
-            var newName = Game.spawns['Spawn1'].createCreep(body[key], '' + roleSpawn + Math.floor(Math.random() * 100), { role: roleSpawn });
-            console.log('Spawn: ' + newName);
+        var roleSpawn = key;
+        for (var i = 0; i < number[key].length; i++) {
+            var maxNum = number[key][i],
+                roleNumber = _.filter(Game.creeps, function (creep) {
+                return creep.memory.role == roleSpawn;
+            }).length;
+            if (roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key])) {
+                var newName = Game.spawns['Spawn1'].createCreep(body[key], '' + roleSpawn + Math.floor(Math.random() * 100), { role: roleSpawn, source: i });
+                console.log('Spawn: ' + newName);
+            }
         }
     };
 
--- src/task.spawn.js
+++ src/task.spawn.js
@@ -1,18 +1,22 @@
-const taskSpawn = (number,body) => {
+const taskSpawn = (number, body) => {
 
     for (let key in number) {
 
-        const roleSpawn = key,
-            maxNum = number[key],
-            roleNumber = _.filter(Game.creeps, (creep) => creep.memory.role == roleSpawn).length;
-        if (roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key])) {
-            const newName = Game.spawns['Spawn1'].createCreep(
-                body[key],
-                `${roleSpawn}${Math.floor(Math.random() * 100)}`,
-                {role: roleSpawn}
-            );
-            console.log('Spawn: ' + newName);
+        const roleSpawn = key;
+        for (let i = 0; i < number[key].length; i++) {
+            let maxNum = number[key][i],
+                roleNumber = _.filter(Game.creeps, (creep) => creep.memory.role == roleSpawn).length;
+            if (roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(body[key])) {
+                const newName = Game.spawns['Spawn1'].createCreep(
+                    body[key],
+                    `${roleSpawn}${Math.floor(Math.random() * 100)}`,
+                    {role: roleSpawn, source: i}
+                );
+                console.log('Spawn: ' + newName);
+            }
         }
+
+
     }
 }
 
