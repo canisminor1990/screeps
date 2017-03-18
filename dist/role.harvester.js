c2dcdf0491673acdf49159e4d1b97a395bc256e0
canisminor1990 <canisminor1990@gmail.com>
2017-03-18 22:30:26 +0800

    'updata'

--- dist/role.harvester.js
+++ dist/role.harvester.js
@@ -10,23 +10,8 @@ var roleHarvester = {
 	run: function run(creep, targets) {
 
 		if (creep.carry.energy < creep.carryCapacity) {
-			var sources = creep.room.find(FIND_SOURCES);
-			var source = sources[0];
-			switch (creep.memory.role) {
-				case 'harvester':
-					source = sources[1];
-					break;
-				case 'upgrader':
-					source = sources[0];
-					break;
-				case 'builder':
-					source = sources[0];
-					break;
-			}
-
-			if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
-			}
+			var sources = (0, _task.taskFindMiner)(creep.memory.source);
+			creep.moveTo(sources, { visualizePathStyle: { stroke: '#ffaa00' } });
 		} else {
 			if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 				creep.moveTo(targets, { visualizePathStyle: { stroke: '#ffffff' } });
