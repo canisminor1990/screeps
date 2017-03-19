06bcb3bbb8c1a11558c5e07494627495dfbd7318
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 08:53:33 +0800

    updata

--- src/task.findMiner.js
+++ src/task.findMiner.js
@@ -14,13 +14,8 @@ const taskFindMiner = (creep) => {
 		}
 	}
 
-	if (minerTarget.carry.energy < minerTarget.carryCapacity) {
-		creep.say('harvest');
-		return (creep.harvest(rawSource) === ERR_NOT_IN_RANGE) ? creep.moveTo(rawSource, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
-	} else {
-		creep.say(minerTarget);
-		return creep.moveTo(minerTarget, {visualizePathStyle: {stroke: '#ffaa00'}});
-	}
+	creep.moveTo(minerTarget, {visualizePathStyle: {stroke: '#ffaa00'}});
+
 }
 
 export default taskFindMiner;
