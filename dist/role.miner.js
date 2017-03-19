fb53cd1de4cd2235d26cc89b857113063c3efe3a
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 11:33:28 +0800

    updata

--- dist/role.miner.js
+++ dist/role.miner.js
@@ -6,13 +6,16 @@ Object.defineProperty(exports, "__esModule", {
 var mySpawn = Game.spawns['Spawn1'];
 var roleMiner = {
 	run: function run(creep) {
-		var pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 1);
-		if (pickup) {
-			console.log('[Pickup]: ' + pickup[0] + creep.pickup(pickup[0]));
-		}
+
 		if (creep.carry.energy < creep.carryCapacity) {
 			var source = mySpawn.room.memory.source[creep.memory.source];
-			creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
+			var pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 1);
+			if (pickup) {
+				creep.say('pickup');
+				creep.pickup(pickup[0]);
+			} else {
+				creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
+			}
 		}
 		if (creep.carry.energy >= 50) {
 			var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
--- src/role.miner.js
+++ src/role.miner.js
@@ -1,13 +1,16 @@
 const mySpawn = Game.spawns['Spawn1'];
 const roleMiner = {
 	run: (creep) => {
-		const pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 1);
-		if (pickup) {
-			console.log('[Pickup]: ' + pickup[0] + creep.pickup(pickup[0]))
-		}
+
 		if (creep.carry.energy < creep.carryCapacity) {
 			const source = mySpawn.room.memory.source[creep.memory.source];
-			(creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
+			const pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 1);
+			if (pickup) {
+				creep.say('pickup')
+				creep.pickup(pickup[0])
+			} else {
+				(creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
+			}
 		}
 		if (creep.carry.energy >= 50) {
 			const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
