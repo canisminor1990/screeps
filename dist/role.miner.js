6086dabfcfdb937035c6c0a5ad5eef80d14e40a4
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 08:32:37 +0800

    updata

--- dist/role.miner.js
+++ dist/role.miner.js
@@ -1,21 +1,23 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 var roleMiner = {
-    run: function run(creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
-            var source = creep.room.find(FIND_SOURCES)[creep.memory.source];
-            creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
-        } else {
-            var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
-            for (var i = 0; i < targets.length; i++) {
-                var num = targets[i].carryCapacity - targets[i].carry.energy;
-                creep.transfer(targets[i], RESOURCE_ENERGY, num > creep.carry.energy ? creep.carry.energy : num);
-            }
-        }
-    }
+	run: function run(creep) {
+		if (creep.carry.energy < creep.carryCapacity) {
+			var source = creep.room.find(FIND_SOURCES)[creep.memory.source];
+			creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
+		} else {
+			var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
+			for (var i = 0; i < targets.length; i++) {
+				if (targets.memory.role != 'miner') {
+					var num = targets[i].carryCapacity - targets[i].carry.energy;
+					creep.transfer(targets[i], RESOURCE_ENERGY, num > creep.carry.energy ? creep.carry.energy : num);
+				}
+			}
+		}
+	}
 };
 
 exports.default = roleMiner;
--- src/role.miner.js
+++ src/role.miner.js
@@ -1,18 +1,20 @@
 const roleMiner = {
-    run: (creep) => {
-        if (creep.carry.energy < creep.carryCapacity) {
-            const source = creep.room.find(FIND_SOURCES)[creep.memory.source];
-            (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
-        }
-        else {
-            const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
-            for (let i = 0 ; i < targets.length; i++){
-                let num = targets[i].carryCapacity - targets[i].carry.energy;
-                creep.transfer(targets[i], RESOURCE_ENERGY, (num > creep.carry.energy) ? creep.carry.energy : num)
-            }
+	run: (creep) => {
+		if (creep.carry.energy < creep.carryCapacity) {
+			const source = creep.room.find(FIND_SOURCES)[creep.memory.source];
+			(creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
+		}
+		else {
+			const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1);
+			for (let i = 0; i < targets.length; i++) {
+				if (targets.memory.role != 'miner') {
+					let num = targets[i].carryCapacity - targets[i].carry.energy;
+					creep.transfer(targets[i], RESOURCE_ENERGY, (num > creep.carry.energy) ? creep.carry.energy : num)
+				}
+			}
 
-        }
-    }
+		}
+	}
 };
 
 export default roleMiner;
