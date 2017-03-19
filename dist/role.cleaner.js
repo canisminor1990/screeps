80e53ee74e65712f223654bdd77cadae8e2f31c0
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 15:12:44 +0800

    'updata'

--- dist/role.cleaner.js
+++ dist/role.cleaner.js
@@ -7,24 +7,24 @@ var mySpawn = Game.spawns['Spawn1'];
 
 exports.default = function (creep, targets, pickup) {
 
-    if (!creep.memory.pickup && pickup) {
+    if (!creep.memory.pickup && pickup && creep.carry.energy < creep.carryCapacity) {
         creep.say('pickup');
     }
 
     if (creep.carry.energy < creep.carryCapacity && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {
         creep.moveTo(pickup, { visualizePathStyle: { reusePathL: 8, stroke: '#44b336' } });
         creep.memory.pickup = true;
-    } else {
+    } else if (targets) {
         creep.memory.pickup = false;
-        if (targets && creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets, { visualizePathStyle: { reusePathL: 8, stroke: '#ffffff' } });
-        } else {
-            var targetsContainer = mySpawn.room.memory.structures.filter(function (structure) {
-                return structure.structureType == STRUCTURE_CONTAINER && structure.store['energy'] < structure.storeCapacity;
-            })[0];
-            if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetsContainer, { visualizePathStyle: { reusePathL: 8, stroke: '#ffffff' } });
-            }
+        if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets, { reusePathL: 8, visualizePathStyle: { stroke: '#ffffff' } });
+        }
+    } else {
+        var targetsContainer = mySpawn.room.memory.structures.filter(function (structure) {
+            return structure.structureType == STRUCTURE_CONTAINER && structure.store["energy"] < structure.storeCapacity;
+        })[0];
+        if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targetsContainer, { reusePathL: 8, visualizePathStyle: { stroke: '#ffffff' } });
         }
     }
 };
--- src/role.cleaner.js
+++ src/role.cleaner.js
@@ -2,25 +2,25 @@ const mySpawn = Game.spawns['Spawn1'];
 
 export default (creep, targets, pickup) => {
 
-    if (!creep.memory.pickup && pickup) {
+    if (!creep.memory.pickup && pickup && creep.carry.energy < creep.carryCapacity) {
         creep.say('pickup')
     }
 
     if (creep.carry.energy < creep.carryCapacity && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(pickup, {visualizePathStyle: {reusePathL: 8,stroke: '#44b336'}});
+        creep.moveTo(pickup, {visualizePathStyle: {reusePathL: 8, stroke: '#44b336'}});
         creep.memory.pickup = true;
-    } else {
+    } else if (targets) {
         creep.memory.pickup = false;
-        if (targets && creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets, {visualizePathStyle: {reusePathL: 8,stroke: '#ffffff'}});
-        } else {
-            const targetsContainer = mySpawn.room.memory.structures.filter(structure => (
-                    structure.structureType == STRUCTURE_CONTAINER
-                ) && structure.store['energy'] < structure.storeCapacity
-            )[0]
-            if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetsContainer, {visualizePathStyle: {reusePathL: 8,stroke: '#ffffff'}});
-            }
+        if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets, {reusePathL: 8, visualizePathStyle: {stroke: '#ffffff'}});
+        }
+    } else {
+        const targetsContainer = mySpawn.room.memory.structures.filter(structure => (
+                structure.structureType == STRUCTURE_CONTAINER
+            ) && structure.store["energy"] < structure.storeCapacity
+        )[0]
+        if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targetsContainer, {reusePathL: 8, visualizePathStyle: {stroke: '#ffffff'}});
         }
     }
 };
