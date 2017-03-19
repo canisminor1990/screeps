f194f79d3822109e7bd93d75efc21f9a9a971b35
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 20:53:31 +0800

    'updata'

--- dist/role.claim.js
+++ dist/role.claim.js
@@ -8,11 +8,10 @@ exports.default = function (creep) {
     "use strict";
 
     var controller = Game.getObjectById('5873bc3511e3e4361b4d738f');
-    creep.moveTo(controller, {
+    creep.reserveController(controller) == ERR_NOT_IN_RANGE ? creep.moveTo(controller, {
         reusePath: 8,
         visualizePathStyle: { stroke: '#ffffff' }
-    });
-    console.log(creep.reserveController(controller));
+    }) : null;
 };
 
 module.exports = exports['default'];
--- src/role.claim.js
+++ src/role.claim.js
@@ -1,10 +1,8 @@
 export default (creep) => {
     "use strict";
     const controller = Game.getObjectById('5873bc3511e3e4361b4d738f');
-    creep.moveTo(controller, {
-        reusePath: 8,
-        visualizePathStyle: {stroke: '#ffffff'}
-    })
-    console.log(creep.reserveController(controller))
-
+    (creep.reserveController(controller) == ERR_NOT_IN_RANGE) ? creep.moveTo(controller, {
+            reusePath: 8,
+            visualizePathStyle: {stroke: '#ffffff'}
+        }) : null;
 }
