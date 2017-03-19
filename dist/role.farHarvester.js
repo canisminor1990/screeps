6b05122c31a37bbaf6d10a59e60d4dcbd2edaefd
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 18:51:58 +0800

    'updata'

--- dist/role.farHarvester.js
+++ dist/role.farHarvester.js
@@ -9,7 +9,6 @@ exports.default = function (creep) {
         var room = 'W81S66';
         var source = creep.pos.findClosestByPath(FIND_SOURCES);
         creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
-        console.log(creep.harvest(source));
     } else {
         creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ? creep.moveTo(Game.spawns['Spawn1']) : null;
     }
--- src/role.farHarvester.js
+++ src/role.farHarvester.js
@@ -3,7 +3,7 @@ export default (creep) => {
         const room = 'W81S66';
         const source = creep.pos.findClosestByPath(FIND_SOURCES);
         (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
-        console.log(creep.harvest(source))
+
     }
     else {
         (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) ? creep.moveTo(Game.spawns['Spawn1']) : null;
