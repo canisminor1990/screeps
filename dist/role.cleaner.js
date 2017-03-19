ef6706aa95389eb7d84fce7486d6e190f2f89d80
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 13:00:18 +0800

    'updata'

--- dist/role.cleaner.js
+++ dist/role.cleaner.js
@@ -3,20 +3,27 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-
-
 var roleCleaner = {
     run: function run(creep, targets, pickup) {
-        if (creep.carry.energy < creep.carryCapacity && pickup && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {} else if (targets) {
-            if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets, { visualizePathStyle: { stroke: '#ffffff' } });
-            }
+
+        if (!creep.memory.pickup && pickup) {
+            creep.say('pickup');
+        }
+
+        if (creep.carry.energy < creep.carryCapacity && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(pickup, { visualizePathStyle: { stroke: '#44b336' } });
+            creep.memory.pickup = true;
         } else {
-            var targetsContainer = mySpawn.room.memory.structures.filter(function (structure) {
-                return structure.structureType == STRUCTURE_CONTAINER && structure.store < structure.storeCapacity;
-            });
-            if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetsContainer, { visualizePathStyle: { stroke: '#ffffff' } });
+            creep.memory.pickup = false;
+            if (targets && creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets, { visualizePathStyle: { stroke: '#ffffff' } });
+            } else {
+                var targetsContainer = mySpawn.room.memory.structures.filter(function (structure) {
+                    return structure.structureType == STRUCTURE_CONTAINER && structure.store < structure.storeCapacity;
+                });
+                if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targetsContainer, { visualizePathStyle: { stroke: '#ffffff' } });
+                }
             }
         }
     }
--- src/role.cleaner.js
+++ src/role.cleaner.js
@@ -1,20 +1,25 @@
-
-
 const roleCleaner = {
-    run: (creep, targets,pickup) => {
-        if (creep.carry.energy < creep.carryCapacity && pickup && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {
+    run: (creep, targets, pickup) => {
 
-        } else if (targets) {
-            if (creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
-            }
+        if (!creep.memory.pickup && pickup) {
+            creep.say('pickup')
+        }
+
+        if (creep.carry.energy < creep.carryCapacity && creep.pickup(pickup) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(pickup, {visualizePathStyle: {stroke: '#44b336'}});
+            creep.memory.pickup = true;
         } else {
-            const targetsContainer = mySpawn.room.memory.structures.filter(structure => (
-                    structure.structureType == STRUCTURE_CONTAINER
-                ) && structure.store < structure.storeCapacity
-            )
-            if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targetsContainer, {visualizePathStyle: {stroke: '#ffffff'}});
+            creep.memory.pickup = false;
+            if (targets && creep.transfer(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets, {visualizePathStyle: {stroke: '#ffffff'}});
+            } else {
+                const targetsContainer = mySpawn.room.memory.structures.filter(structure => (
+                        structure.structureType == STRUCTURE_CONTAINER
+                    ) && structure.store < structure.storeCapacity
+                )
+                if (targetsContainer && creep.transfer(targetsContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targetsContainer, {visualizePathStyle: {stroke: '#ffffff'}});
+                }
             }
         }
     }
