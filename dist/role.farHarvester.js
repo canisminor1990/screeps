cb5a2c7cb847fe1aeb5893ddcd53efef7e053368
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 18:46:31 +0800

    'updata'

--- dist/role.farHarvester.js
+++ dist/role.farHarvester.js
@@ -7,20 +7,8 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (creep) {
     if (creep.carry.energy < creep.carryCapacity) {
         var room = 'W82S67';
-        if (creep.room.name !== room) {
-            creep.moveTo(new RoomPosition(39, 39, room));
-        } else {
-            var source = creep.findClosestByPath(FIND_SOURCES);
-            var pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 4);
-            if (pickup.length > 0) {
-                creep.pickup(pickup[0]) == ERR_NOT_IN_RANGE ? creep.moveTo(pickup[0], {
-                    reusePath: 8,
-                    visualizePathStyle: { stroke: '#ffaa00' }
-                }) : null;
-            } else {
-                creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
-            }
-        }
+        var source = creep.findClosestByPath(FIND_SOURCES);
+        creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
     } else {
         creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ? creep.moveTo(Game.spawns['Spawn1']) : null;
     }
--- src/role.farHarvester.js
+++ src/role.farHarvester.js
@@ -1,20 +1,9 @@
 export default (creep) => {
     if (creep.carry.energy < creep.carryCapacity) {
-        const room = 'W82S67'
-        if (creep.room.name !== room) {
-            creep.moveTo(new RoomPosition(39, 39, room));
-        } else {
-            const source = creep.findClosestByPath(FIND_SOURCES)
-            const pickup = creep.pos.findInRange(FIND_DROPPED_ENERGY, 4);
-            if (pickup.length > 0) {
-                (creep.pickup(pickup[0]) == ERR_NOT_IN_RANGE) ? creep.moveTo(pickup[0], {
-                        reusePath: 8,
-                        visualizePathStyle: {stroke: '#ffaa00'}
-                    }) : null;
-            } else {
-                (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
-            }
-        }
+        const room = 'W82S67';
+        const source = creep.findClosestByPath(FIND_SOURCES);
+        (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
+
     }
     else {
         (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) ? creep.moveTo(Game.spawns['Spawn1']) : null;
