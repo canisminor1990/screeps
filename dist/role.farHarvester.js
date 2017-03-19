19c9e2cb4e382dac01939810f39726f4666590df
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 18:54:00 +0800

    'updata'

--- dist/role.farHarvester.js
+++ dist/role.farHarvester.js
@@ -8,7 +8,7 @@ exports.default = function (creep) {
     if (creep.carry.energy < creep.carryCapacity) {
         var room = 'W81S66';
         var source = creep.pos.findClosestByPath(FIND_SOURCES);
-        creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(27, 21, room)) : null;
+        creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(27, 21, room), { reusePath: 8, visualizePathStyle: { stroke: '#ffffff' } }) : null;
     } else {
         creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ? creep.moveTo(Game.spawns['Spawn1']) : null;
     }
--- src/role.farHarvester.js
+++ src/role.farHarvester.js
@@ -2,7 +2,7 @@ export default (creep) => {
     if (creep.carry.energy < creep.carryCapacity) {
         const room = 'W81S66';
         const source = creep.pos.findClosestByPath(FIND_SOURCES);
-        (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(27, 21, room)) : null;
+        (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(27, 21, room), {reusePath: 8, visualizePathStyle: {stroke: '#ffffff'}}) : null;
 
     }
     else {
