69df7ffde1410a0d6445294bd31090b2ef04a4a1
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 15:02:19 +0800

    'updata'

--- dist/task.findMiner.js
+++ dist/task.findMiner.js
@@ -6,25 +6,33 @@ Object.defineProperty(exports, "__esModule", {
 var mySpawn = Game.spawns['Spawn1'];
 
 var taskFindMiner = function taskFindMiner(creep) {
-    var minerTarget = void 0,
-        minerEnergy = 0;
-    var miner = mySpawn.room.memory.miner.filter(function (miner) {
-        return creep.memory.source === miner.memory.source;
-    });
-    for (var i = 0; i < miner.length; i++) {
-        if (minerEnergy < miner[i].carry.energy) {
-            minerTarget = miner[i];
-            minerEnergy = miner[i].carry.energy;
+    var source = mySpawn.room.memory.source[creep.memory.source];
+    if (source.energy != 0) {
+        var minerTarget = void 0,
+            minerEnergy = 0;
+        var miner = mySpawn.room.memory.miner.filter(function (miner) {
+            return creep.memory.source === miner.memory.source;
+        });
+        for (var i = 0; i < miner.length; i++) {
+            if (minerEnergy < miner[i].carry.energy) {
+                minerTarget = miner[i];
+                minerEnergy = miner[i].carry.energy;
+            }
         }
-    }
-    if (minerTarget && minerEnergy >= 50) {
-        creep.moveTo(minerTarget, { reusePathL: 8, visualizePathStyle: { stroke: '#ffaa00' } });
+
+        if (minerTarget && minerEnergy >= 50) {
+            creep.moveTo(minerTarget, { reusePathL: 8, visualizePathStyle: { stroke: '#ffaa00' } });
+        } else {
+            creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, {
+                reusePathL: 8,
+                visualizePathStyle: { stroke: '#ffaa00' }
+            }) : null;
+        }
     } else {
-        var source = mySpawn.room.memory.source[creep.memory.source];
-        creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, {
-            reusePathL: 8,
-            visualizePathStyle: { stroke: '#ffaa00' }
-        }) : null;
+        var targetsContainer = mySpawn.room.memory.structures.filter(function (structure) {
+            return structure.structureType == STRUCTURE_CONTAINER && structure.store["energy"] < structure.storeCapacity;
+        })[0];
+        creep.moveTo(targetsContainer, { reusePathL: 8, visualizePathStyle: { stroke: '#ffffff' } });
     }
 };
 
--- src/task.findMiner.js
+++ src/task.findMiner.js
@@ -1,24 +1,32 @@
 const mySpawn = Game.spawns['Spawn1'];
 
 const taskFindMiner = (creep) => {
-    let minerTarget, minerEnergy = 0;
-    const miner = mySpawn.room.memory.miner.filter(miner => creep.memory.source === miner.memory.source)
-    for (let i = 0; i < miner.length; i++) {
-        if (minerEnergy < miner[i].carry.energy) {
-            minerTarget = miner[i];
-            minerEnergy = miner[i].carry.energy
+    const source = mySpawn.room.memory.source[creep.memory.source];
+    if (source.energy != 0) {
+        let minerTarget, minerEnergy = 0;
+        const miner = mySpawn.room.memory.miner.filter(miner => creep.memory.source === miner.memory.source)
+        for (let i = 0; i < miner.length; i++) {
+            if (minerEnergy < miner[i].carry.energy) {
+                minerTarget = miner[i];
+                minerEnergy = miner[i].carry.energy
+            }
         }
-    }
-    if (minerTarget && minerEnergy >= 50) {
-        creep.moveTo(minerTarget, {reusePathL: 8, visualizePathStyle: {stroke: '#ffaa00'}});
-    } else {
-        const source = mySpawn.room.memory.source[creep.memory.source];
-        (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {
-                reusePathL: 8,
-                visualizePathStyle: {stroke: '#ffaa00'}
-            }) : null;
-    }
 
+        if (minerTarget && minerEnergy >= 50) {
+            creep.moveTo(minerTarget, {reusePathL: 8, visualizePathStyle: {stroke: '#ffaa00'}});
+        } else {
+            (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {
+                    reusePathL: 8,
+                    visualizePathStyle: {stroke: '#ffaa00'}
+                }) : null;
+        }
+    } else {
+        const targetsContainer = mySpawn.room.memory.structures.filter(structure => (
+                structure.structureType == STRUCTURE_CONTAINER
+            ) && structure.store["energy"] < structure.storeCapacity
+        )[0]
+        creep.moveTo(targetsContainer, {reusePathL: 8, visualizePathStyle: {stroke: '#ffffff'}});
+    }
 }
 
 export default taskFindMiner;
