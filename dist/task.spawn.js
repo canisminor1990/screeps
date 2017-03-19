2f1fb7648a60df07ad57d51df1e65edce17a5bfb
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 13:11:38 +0800

    'updata'

--- dist/task.spawn.js
+++ dist/task.spawn.js
@@ -1,66 +1,69 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+    value: true
 });
 var mySpawn = Game.spawns['Spawn1'];
 
 var taskSpawn = function taskSpawn(number, body) {
 
-	for (var name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			delete Memory.creeps[name];
-			console.log('Clearing non-existing creep memory:', name);
-		}
-	}
+    if (mySpawn.energy >= 300) {
 
-	var _loop = function _loop(key) {
-		var roleSpawn = key;
+        for (var name in Memory.creeps) {
+            if (!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
 
-		var _loop2 = function _loop2(i) {
-			var maxNum = number[key][i];
-			var roleNumber = _.filter(Game.creeps, function (creep) {
-				return creep.memory.role == roleSpawn && creep.memory.source == i;
-			}).length;
-			var roleBody = buildBody(body[key]);
+        var _loop = function _loop(key) {
+            var roleSpawn = key;
 
-			if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(roleBody) === OK) {
-				var _name = '[' + roleSpawn + ']' + getNowFormatDate();
-				Game.spawns['Spawn1'].createCreep(roleBody, _name, { role: roleSpawn, source: i });
-				console.log(['Spawn:', _name, 'Source:', i].join(' '));
-			}
-		};
+            var _loop2 = function _loop2(i) {
+                var maxNum = number[key][i];
+                var roleNumber = _.filter(Game.creeps, function (creep) {
+                    return creep.memory.role == roleSpawn && creep.memory.source == i;
+                }).length;
+                var roleBody = buildBody(body[key]);
 
-		for (var i = 0; i < number[key].length; i++) {
-			_loop2(i);
-		}
-	};
+                if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(roleBody) === OK) {
+                    var _name = '[' + roleSpawn + ']' + getNowFormatDate();
+                    Game.spawns['Spawn1'].createCreep(roleBody, _name, { role: roleSpawn, source: i });
+                    console.log(['Spawn:', _name, 'Source:', i].join(' '));
+                }
+            };
 
-	for (var key in number) {
-		_loop(key);
-	}
+            for (var i = 0; i < number[key].length; i++) {
+                _loop2(i);
+            }
+        };
 
-	if (mySpawn.spawning) {
-		var spawningCreep = Game.creeps[mySpawn.spawning.name];
-		mySpawn.room.visual.text('[Spawn]' + spawningCreep.memory.role, mySpawn.pos.x + 1, mySpawn.pos.y, { align: 'left', opacity: 0.8 });
-	}
+        for (var key in number) {
+            _loop(key);
+        }
+    }
+
+    if (mySpawn.spawning) {
+        var spawningCreep = Game.creeps[mySpawn.spawning.name];
+        mySpawn.room.visual.text('[Spawn]' + spawningCreep.memory.role, mySpawn.pos.x + 1, mySpawn.pos.y, { align: 'left', opacity: 0.8 });
+    }
 };
 
 exports.default = taskSpawn;
 
 
 function getNowFormatDate() {
-	var date = new Date();
-	return [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
+    var date = new Date();
+    return [date.getHours(), date.getMinutes(), date.getSeconds()].join(':');
 }
 
 function buildBody(obj) {
-	var array = [];
-	for (var key in obj) {
-		for (var num = 0; num < obj[key]; num++) {
-			array.push(key);
-		}
-	}
-	return array;
+    var array = [];
+    for (var key in obj) {
+        for (var num = 0; num < obj[key]; num++) {
+            array.push(key);
+        }
+    }
+    return array;
 }
 module.exports = exports['default'];
--- src/task.spawn.js
+++ src/task.spawn.js
@@ -2,60 +2,64 @@ const mySpawn = Game.spawns['Spawn1'];
 
 const taskSpawn = (number, body) => {
 
-	for (let name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			delete Memory.creeps[name];
-			console.log('Clearing non-existing creep memory:', name);
-		}
-	}
+    if (mySpawn.energy >= 300) {
 
-	for (let key in number) {
-		const roleSpawn = key;
-		for (let i = 0; i < number[key].length; i++) {
-			const maxNum = number[key][i]
-			const roleNumber = _.filter(Game.creeps, (creep) => creep.memory.role == roleSpawn && creep.memory.source == i).length;
-			const roleBody = buildBody(body[key])
+        for (let name in Memory.creeps) {
+            if (!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
+        }
 
-			if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(roleBody) === OK) {
-				const name = `[${roleSpawn}]${getNowFormatDate()}`
-				Game.spawns['Spawn1'].createCreep(
-						roleBody,
-						name,
-						{role: roleSpawn, source: i}
-				);
-				console.log(['Spawn:',
-				             name,
-				             'Source:',
-				             i].join(' '));
-			}
-		}
-	}
+        for (let key in number) {
+            const roleSpawn = key;
+            for (let i = 0; i < number[key].length; i++) {
+                const maxNum = number[key][i]
+                const roleNumber = _.filter(Game.creeps, (creep) => creep.memory.role == roleSpawn && creep.memory.source == i).length;
+                const roleBody = buildBody(body[key])
 
-	if (mySpawn.spawning) {
-		const spawningCreep = Game.creeps[mySpawn.spawning.name];
-		mySpawn.room.visual.text(
-				'[Spawn]' + spawningCreep.memory.role,
-				mySpawn.pos.x + 1,
-				mySpawn.pos.y,
-				{align: 'left', opacity: 0.8});
-	}
+                if (number[key][i] > 0 && roleNumber < maxNum && Game.spawns['Spawn1'].canCreateCreep(roleBody) === OK) {
+                    const name = `[${roleSpawn}]${getNowFormatDate()}`
+                    Game.spawns['Spawn1'].createCreep(
+                        roleBody,
+                        name,
+                        {role: roleSpawn, source: i}
+                    );
+                    console.log(['Spawn:',
+                        name,
+                        'Source:',
+                        i].join(' '));
+                }
+            }
+        }
+
+    }
+
+    if (mySpawn.spawning) {
+        const spawningCreep = Game.creeps[mySpawn.spawning.name];
+        mySpawn.room.visual.text(
+            '[Spawn]' + spawningCreep.memory.role,
+            mySpawn.pos.x + 1,
+            mySpawn.pos.y,
+            {align: 'left', opacity: 0.8});
+    }
 }
 
 export default taskSpawn;
 
 function getNowFormatDate() {
-	const date = new Date();
-	return [date.getHours(),
-	        date.getMinutes(),
-	        date.getSeconds()].join(':')
+    const date = new Date();
+    return [date.getHours(),
+        date.getMinutes(),
+        date.getSeconds()].join(':')
 }
 
 function buildBody(obj) {
-	let array = [];
-	for (let key in obj) {
-		for (let num = 0; num < obj[key]; num++) {
-			array.push(key)
-		}
-	}
-	return array;
+    let array = [];
+    for (let key in obj) {
+        for (let num = 0; num < obj[key]; num++) {
+            array.push(key)
+        }
+    }
+    return array;
 }
