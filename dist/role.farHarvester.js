725150ad0c03f22f5129425ea1cd13432addd355
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 18:55:19 +0800

    'updata'

--- dist/role.farHarvester.js
+++ dist/role.farHarvester.js
@@ -7,8 +7,15 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (creep) {
     if (creep.carry.energy < creep.carryCapacity) {
         var room = 'W81S66';
-        var source = creep.pos.findClosestByPath(FIND_SOURCES);
-        creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(27, 21, room), { reusePath: 8, visualizePathStyle: { stroke: '#ffffff' } }) : null;
+        if (creep.room.name == room) {
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(27, 21, room), {
+                reusePath: 8,
+                visualizePathStyle: { stroke: '#ffffff' }
+            }) : null;
+        } else {
+            creep.moveTo(new RoomPosition(27, 21, room), { reusePath: 8, visualizePathStyle: { stroke: '#ffffff' } });
+        }
     } else {
         creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ? creep.moveTo(Game.spawns['Spawn1']) : null;
     }
--- src/role.farHarvester.js
+++ src/role.farHarvester.js
@@ -1,8 +1,15 @@
 export default (creep) => {
     if (creep.carry.energy < creep.carryCapacity) {
         const room = 'W81S66';
-        const source = creep.pos.findClosestByPath(FIND_SOURCES);
-        (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(27, 21, room), {reusePath: 8, visualizePathStyle: {stroke: '#ffffff'}}) : null;
+        if (creep.room.name == room) {
+            const source = creep.pos.findClosestByPath(FIND_SOURCES);
+            (creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(new RoomPosition(27, 21, room), {
+                    reusePath: 8,
+                    visualizePathStyle: {stroke: '#ffffff'}
+                }) : null;
+        } else {
+            creep.moveTo(new RoomPosition(27, 21, room), {reusePath: 8, visualizePathStyle: {stroke: '#ffffff'}})
+        }
 
     }
     else {
