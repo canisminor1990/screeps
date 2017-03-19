e1c0522d44324ffa91cfd059e039d46ef5c04a5e
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 10:48:47 +0800

    updata

--- dist/role.miner.js
+++ dist/role.miner.js
@@ -11,15 +11,16 @@ var roleMiner = {
 			var source = mySpawn.room.memory.source[creep.memory.source];
 			creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } }) : null;
 		}
-
-		var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
-			filter: function filter(tCreep) {
-				return tCreep.memory.role !== 'miner';
+		if (creep.carry.energy >= 50) {
+			var targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
+				filter: function filter(tCreep) {
+					return tCreep.memory.role !== 'miner';
+				}
+			});
+			for (var name in targets) {
+				var num = targets[name].carryCapacity - targets[name].carry.energy;
+				creep.transfer(targets[name], RESOURCE_ENERGY, num > creep.carry.energy ? creep.carry.energy : num);
 			}
-		});
-		for (var name in targets) {
-			var num = targets[name].carryCapacity - targets[name].carry.energy;
-			creep.transfer(targets[name], RESOURCE_ENERGY, num > creep.carry.energy ? creep.carry.energy : num);
 		}
 	}
 };
--- src/role.miner.js
+++ src/role.miner.js
@@ -6,13 +6,14 @@ const roleMiner = {
 			const source = mySpawn.room.memory.source[creep.memory.source];
 			(creep.harvest(source) == ERR_NOT_IN_RANGE) ? creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}}) : null;
 		}
-
-		const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
-			filter: tCreep => tCreep.memory.role !== 'miner'
-		});
-		for (let name in targets) {
-			let num = targets[name].carryCapacity - targets[name].carry.energy;
-			creep.transfer(targets[name], RESOURCE_ENERGY, (num > creep.carry.energy) ? creep.carry.energy : num)
+		if (creep.carry.energy >= 50) {
+			const targets = creep.pos.findInRange(FIND_MY_CREEPS, 1, {
+				filter: tCreep => tCreep.memory.role !== 'miner'
+			});
+			for (let name in targets) {
+				let num = targets[name].carryCapacity - targets[name].carry.energy;
+				creep.transfer(targets[name], RESOURCE_ENERGY, (num > creep.carry.energy) ? creep.carry.energy : num)
+			}
 		}
 	}
 };
