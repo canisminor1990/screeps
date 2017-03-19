e669e59d1861a950ea048e2c10044416ee3d2b1b
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 09:26:53 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -6,46 +6,49 @@ var _task = require('./task');
 
 module.exports = {
 
-    loop: function loop() {
+	loop: function loop() {
 
-        for (var name in Memory.creeps) {
-            if (!Game.creeps[name]) {
-                delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:', name);
-            }
-        }
+		for (var name in Memory.creeps) {
+			if (!Game.creeps[name]) {
+				delete Memory.creeps[name];
+				console.log('Clearing non-existing creep memory:', name);
+			}
+		}
 
-        (0, _task.taskSpawn)(_role.roleConfig.number, _role.roleConfig.body);
+		(0, _task.taskSpawn)(_role.roleConfig.number, _role.roleConfig.body);
 
-        if (Game.spawns['Spawn1'].spawning) {
-            var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-            Game.spawns['Spawn1'].room.visual.text('[Spawn]' + spawningCreep.memory.role, Game.spawns['Spawn1'].pos.x + 1, Game.spawns['Spawn1'].pos.y, { align: 'left', opacity: 0.8 });
-        }
+		if (Game.spawns['Spawn1'].spawning) {
+			var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+			Game.spawns['Spawn1'].room.visual.text('[Spawn]' + spawningCreep.memory.role, Game.spawns['Spawn1'].pos.x + 1, Game.spawns['Spawn1'].pos.y, { align: 'left', opacity: 0.8 });
+		}
 
-        var targets = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
-            filter: function filter(structure) {
-                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-            }
-        });
+		var targetsHarvest = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+			filter: function filter(structure) {
+				return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+			}
+		});
+		var targetsBuild = Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES);
 
-        for (var _name in Game.creeps) {
-            var creep = Game.creeps[_name];
+		for (var _name in Game.creeps) {
+			var creep = Game.creeps[_name];
 
-            switch (creep.memory.role) {
-                case 'harvester':
-                    targets.length > 0 ? _role.roleHarvester.run(creep, targets[0]) : _role.roleBuilder.run(creep);
-                    break;
-                case 'upgrader':
-                    _role.roleUpgrader.run(creep);
-                    break;
-                case 'builder':
-                    _role.roleBuilder.run(creep);
-                    break;
-                case 'miner':
-                    _role.roleMiner.run(creep);
-                    break;
-            }
-        }
-    }
+			switch (creep.memory.role) {
+				case 'harvester':
+					targetsHarvest.length > 0 ? _role.roleHarvester.run(creep, targetsHarvest[0]) : _role.roleBuilder.run(creep);
+					break;
+				case 'upgrader':
+					_role.roleUpgrader.run(creep);
+					break;
+				case 'builder':
+
+					targetsBuild.length > 0 ? _role.roleBuilder.run(creep, targetsBuild[0]) : _role.roleHarvester.run(creep, targetsHarvest[0]);
+
+					break;
+				case 'miner':
+					_role.roleMiner.run(creep);
+					break;
+			}
+		}
+	}
 
 };
--- src/main.js
+++ src/main.js
@@ -1,57 +1,58 @@
-import {roleConfig, roleHarvester, roleUpgrader, roleBuilder,roleMiner} from './role';
+import {roleConfig, roleHarvester, roleUpgrader, roleBuilder, roleMiner} from './role';
 import {taskSpawn} from './task';
-
 
 module.exports = {
 
-    loop: () => {
+	loop: () => {
 
-        for (let name in Memory.creeps) {
-            if (!Game.creeps[name]) {
-                delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:', name);
-            }
-        }
+		for (let name in Memory.creeps) {
+			if (!Game.creeps[name]) {
+				delete Memory.creeps[name];
+				console.log('Clearing non-existing creep memory:', name);
+			}
+		}
 
-        taskSpawn(roleConfig.number, roleConfig.body)
+		taskSpawn(roleConfig.number, roleConfig.body)
 
-        if (Game.spawns['Spawn1'].spawning) {
-            const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-            Game.spawns['Spawn1'].room.visual.text(
-                '[Spawn]' + spawningCreep.memory.role,
-                Game.spawns['Spawn1'].pos.x + 1,
-                Game.spawns['Spawn1'].pos.y,
-                {align: 'left', opacity: 0.8});
-        }
+		if (Game.spawns['Spawn1'].spawning) {
+			const spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+			Game.spawns['Spawn1'].room.visual.text(
+					'[Spawn]' + spawningCreep.memory.role,
+					Game.spawns['Spawn1'].pos.x + 1,
+					Game.spawns['Spawn1'].pos.y,
+					{align: 'left', opacity: 0.8});
+		}
 
+		const targetsHarvest = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
+			filter: (structure) => {
+				return (structure.structureType == STRUCTURE_EXTENSION ||
+						structure.structureType == STRUCTURE_SPAWN ||
+						structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+			}
+		});
+		const targetsBuild = Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES);
 
-        const targets = Game.spawns['Spawn1'].room.find(FIND_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION ||
-                    structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-            }
-        });
+		for (let name in Game.creeps) {
+			const creep = Game.creeps[name];
 
-        for (let name in Game.creeps) {
-            const creep = Game.creeps[name];
+			switch (creep.memory.role) {
+				case 'harvester':
+					(targetsHarvest.length > 0) ?
+							roleHarvester.run(creep, targetsHarvest[0]) : roleBuilder.run(creep);
+					break;
+				case 'upgrader':
+					roleUpgrader.run(creep);
+					break;
+				case 'builder':
 
-            switch (creep.memory.role) {
-                case 'harvester':
-                    (targets.length > 0) ?
-                        roleHarvester.run(creep, targets[0]) : roleBuilder.run(creep);
-                    break;
-                case 'upgrader':
-                    roleUpgrader.run(creep);
-                    break;
-                case 'builder':
-	                roleBuilder.run(creep)
-	                break;
-                case 'miner':
-                    roleMiner.run(creep);
-                    break;
-            }
-        }
-    }
+					(targetsBuild.length > 0) ? roleBuilder.run(creep, targetsBuild[0]) : roleHarvester.run(creep, targetsHarvest[0])
+
+					break;
+				case 'miner':
+					roleMiner.run(creep);
+					break;
+			}
+		}
+	}
 
 }
