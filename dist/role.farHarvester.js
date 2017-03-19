871526bb640764d2b34f0fece0579af5a0370caf
canisminor1990 <canisminor1990@gmail.com>
2017-03-19 18:49:04 +0800

    'updata'

--- dist/role.farHarvester.js
+++ dist/role.farHarvester.js
@@ -9,6 +9,7 @@ exports.default = function (creep) {
         var room = 'W82S67';
         var source = creep.pos.findClosestByPath(FIND_SOURCES);
         creep.harvest(source) == ERR_NOT_IN_RANGE ? creep.moveTo(new RoomPosition(39, 39, room)) : null;
+        console.log(creep.harvest(source));
     } else {
         creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ? creep.moveTo(Game.spawns['Spawn1']) : null;
     }
