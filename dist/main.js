b07bbf1548628310b647f37fdfe28ff6cab0e005
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 22:28:30 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -432,14 +432,16 @@ var Timer = exports.Timer = function () {
 
         this.tick = tick;
         this.func = func;
-        Memory.timer[tick] = Game.time;
+        Memory.timer = {
+            tick: tick,
+            timeout: Game.time
+        };
     }
 
     _createClass(Timer, [{
         key: "run",
         value: function run() {
-            if (Game.time - Memory.timer[this.tick] < this.tick) return;
-            Memory.timer[this.tick] = Game.time;
+
             this.func(this);
         }
     }]);
--- src/_util/Timer.js
+++ src/_util/Timer.js
@@ -3,12 +3,14 @@ export class Timer {
     constructor(tick, func) {
         this.tick = tick;
         this.func = func;
-        Memory.timer[tick] = Game.time;
+        Memory.timer = {
+            tick: tick,
+            timeout: Game.time
+        }
     }
 
     run() {
-        if (Game.time -  Memory.timer[this.tick] < this.tick) return;
-        Memory.timer[this.tick] = Game.time;
+
         this.func(this);
     }
 
