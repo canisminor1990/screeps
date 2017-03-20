60b89d64f18e26f6bdf7172d00b3a824a90e11b7
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 12:32:48 +0800

    sync

--- dist/main.js
+++ dist/main.js
@@ -13,68 +13,67 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 var mySpawn = Game.spawns['Spawn1'];
 module.exports = {
 
-    loop: function loop() {
+	loop: function loop() {
 
-        mySpawn.room.memory = {
-            structures: mySpawn.room.find(FIND_STRUCTURES),
-            constructionSites: mySpawn.room.find(FIND_CONSTRUCTION_SITES),
-            source: mySpawn.room.find(FIND_SOURCES),
-            miner: mySpawn.room.find(FIND_MY_CREEPS, { filter: function filter(miner) {
-                    return miner.memory.role === "miner";
-                } }),
-            drop: mySpawn.room.find(FIND_DROPPED_ENERGY)
-        };
-        var targetsHarvest = mySpawn.room.memory.structures.filter(function (structure) {
-            return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-        });
-        var targetsBuild = mySpawn.room.memory.constructionSites;
-        var targetsPickup = mySpawn.room.memory.drop;
+		mySpawn.room.memory = {
+			structures: mySpawn.room.find(FIND_STRUCTURES),
+			constructionSites: mySpawn.room.find(FIND_CONSTRUCTION_SITES),
+			source: mySpawn.room.find(FIND_SOURCES),
+			miner: mySpawn.room.find(FIND_MY_CREEPS, { filter: function filter(miner) {
+					return miner.memory.role === "miner";
+				} }),
+			drop: mySpawn.room.find(FIND_DROPPED_ENERGY)
+		};
+		var targetsHarvest = mySpawn.room.memory.structures.filter(function (structure) {
+			return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+		});
+		var targetsBuild = mySpawn.room.memory.constructionSites;
+		var targetsPickup = mySpawn.room.memory.drop;
 
-        for (var name in mySpawn.room.memory.structures) {
-            var structureName = mySpawn.room.memory.structures[name];
-            switch (structureName.structureType) {
-                case 'spawn':
-                    structure.spawn(structureName);
-                    break;
-                case 'tower':
-                    structure.tower(structureName);
-                    break;
-                case 'container':
-                    structure.container(structureName, targetsHarvest.length, targetsBuild.length);
+		for (var name in mySpawn.room.memory.structures) {
+			var structureName = mySpawn.room.memory.structures[name];
+			switch (structureName.structureType) {
+				case 'spawn':
+					structure.spawn(structureName);
+					break;
+				case 'tower':
+					structure.tower(structureName);
+					break;
+				case 'container':
+					structure.container(structureName, targetsHarvest.length, targetsBuild.length);
 
-                    break;
-            }
-        }
+					break;
+			}
+		}
 
-        for (var _name in Game.creeps) {
-            var creep = Game.creeps[_name];
-            switch (creep.memory.role) {
-                case 'claim':
-                    role.claim(creep);
-                    break;
-                case 'farMiner':
-                    role.farMiner(creep);
-                    break;
-                case 'farHarvester':
-                    role.farHarvester(creep);
-                    break;
-                case 'harvester':
-                    role.harvester(creep);
-                    break;
-                case 'upgrader':
-                    role.upgrader(creep);
-                    break;
-                case 'builder':
-                    targetsBuild.length > 0 ? role.builder(creep) : role.harvester(creep);
-                    break;
-                case 'miner':
-                    role.miner(creep);
-                    break;
-                case 'cleaner':
-                    targetsPickup.length > 0 ? role.cleaner(creep) : role.harvester(creep);
-                    break;
-            }
-        }
-    }
-
+		for (var _name in Game.creeps) {
+			var creep = Game.creeps[_name];
+			switch (creep.memory.role) {
+				case 'claim':
+					role.claim(creep);
+					break;
+				case 'farMiner':
+					role.farMiner(creep);
+					break;
+				case 'farHarvester':
+					role.farHarvester(creep);
+					break;
+				case 'harvester':
+					role.harvester(creep);
+					break;
+				case 'upgrader':
+					role.upgrader(creep);
+					break;
+				case 'builder':
+					targetsBuild.length > 0 ? role.builder(creep) : role.harvester(creep);
+					break;
+				case 'miner':
+					role.miner(creep);
+					break;
+				case 'cleaner':
+					targetsPickup.length > 0 ? role.cleaner(creep) : role.harvester(creep);
+					break;
+			}
+		}
+	}
 };
