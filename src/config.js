5ed919893ff7a05dbfb31d8a48e1c0960e428558
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 20:31:39 +0800

    'updata'

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ const role = {
     }
 }
 
-const repair = (structure) => (structure.hits / structure.hitsMax) < 0.5 && structure.hits < 100000
+const repair = (structure) => (structure.hits / structure.hitsMax) < 0.5 && structure.hits < 10000
 
 /*
  "move": 50,
