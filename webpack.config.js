6c7281e1969152f07af4c539fcdd0ac7eea16746
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 13:18:18 +0800

    updata

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	entry: './src/main',
+	output: {
+		path: 'dist/',
+		filename: 'main.js',
+		libraryTarget: 'commonjs2',
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				query: {
+					presets: [
+						require.resolve('babel-preset-react'), // React preset is needed only for flow support.
+						require.resolve('babel-preset-es2015'),
+						require.resolve('babel-preset-stage-2'),
+					],
+				},
+			},
+		],
+	},
+};
