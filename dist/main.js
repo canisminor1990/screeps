27f68798606819f57e9d517746443353af2251b9
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 17:09:32 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -854,15 +854,28 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (container, targetsHarvest, targetsBuild) {
 
 	var targets = container.pos.findInRange(FIND_MY_CREEPS, 2, {
-		filter: function filter(tCreep) {
-			return tCreep.memory.role !== 'miner' && tCreep.memory.role !== 'cleaner' && (targetsHarvest > 0 ? tCreep.memory.role !== 'harvester' && tCreep.memory.role !== 'farHarvester' : null) && ((targetsBuild = 0) ? tCreep.memory.role !== 'builder' : null);
+		filter: function filter(creep) {
+			return creep.memory.role !== 'miner' && creep.memory.role !== 'cleaner';
 		}
-	})[0];
+	});
 
-	if (targets) {
-		console.log(targets);
-		container.transfer(targets, RESOURCE_ENERGY);
-		container.room.visual.text('[Transfer]', container.pos.x + 1, container.pos.y, { align: 'left', opacity: 0.8 });
+	if (targetsHarvest == 0) {
+		targets = targets.filter(function (creep) {
+			return creep.memory.role !== 'harvester' && creep.memory.role !== 'farHarvester';
+		});
+	}
+
+	if (targetsBuild == 0) {
+		targets = targets.filter(function (creep) {
+			return creep.memory.role !== 'builder';
+		});
+	}
+
+	if (targets[0]) {
+		console.log(targets[0]);
+		if (container.transfer(targets[0], RESOURCE_ENERGY) == OK) {
+			container.room.visual.text('[Transfer]', container.pos.x + 1, container.pos.y, { align: 'left', opacity: 0.8 });
+		}
 	}
 };
 
--- src/structure/_container.js
+++ src/structure/_container.js
@@ -1,22 +1,29 @@
 export default (container, targetsHarvest, targetsBuild) => {
 
-	const targets = container.pos.findInRange(FIND_MY_CREEPS, 2, {
-		filter: tCreep =>
-		tCreep.memory.role !== 'miner' &&
-		tCreep.memory.role !== 'cleaner' &&
-		((targetsHarvest > 0) ? (tCreep.memory.role !== 'harvester' && tCreep.memory.role !== 'farHarvester') : null) &&
-		((targetsBuild = 0) ? tCreep.memory.role !== 'builder' : null)
-	})[0];
+	let targets = container.pos.findInRange(FIND_MY_CREEPS, 2, {
+		filter: creep =>
+		creep.memory.role !== 'miner' &&
+		creep.memory.role !== 'cleaner'
+	});
 
+	if (targetsHarvest == 0) {
+		targets = targets.filter(creep => creep.memory.role !== 'harvester' &&
+		                                  creep.memory.role !== 'farHarvester'
+		)
+	}
 
+	if (targetsBuild == 0) {
+		targets = targets.filter(creep => creep.memory.role !== 'builder')
+	}
 
-	if (targets) {
-		console.log(targets)
-		container.transfer(targets, RESOURCE_ENERGY);
-		container.room.visual.text(
+	if (targets[0]) {
+		console.log(targets[0])
+		if (container.transfer(targets[0], RESOURCE_ENERGY) == OK) {
+			container.room.visual.text(
 				'[Transfer]',
 				container.pos.x + 1,
 				container.pos.y,
 				{align: 'left', opacity: 0.8});
+		}
 	}
 }
