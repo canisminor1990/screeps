79bc25f10289c41b3ad148e06bf6bfb97acc8908
canisminor1990 <canisminor1990@gmail.com>
2017-03-20 23:31:44 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -420,7 +420,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.Timer = Timer;
 function Timer(tick) {
-    if (Memory.timer[tick] && Game.time - Memory.timer[tick] < this.tick) return false;
+    if (Memory.timer[tick] && Game.time - Memory.timer[tick] < tick) return false;
     Memory.timer[tick] = Game.time;
     return true;
 }
--- src/_util/Timer.js
+++ src/_util/Timer.js
@@ -1,5 +1,5 @@
 export function Timer (tick) {
-    if (Memory.timer[tick] && Game.time - Memory.timer[tick] < this.tick) return false;
+    if (Memory.timer[tick] && Game.time - Memory.timer[tick] < tick) return false;
     Memory.timer[tick] = Game.time;
     return true
 }
