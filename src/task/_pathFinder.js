df8a2f37b7716266abaabd96f8e303ecaf002ddb
canisminor1990 <canisminor1990@gmail.com>
2017-03-21 22:53:09 +0800

    'updata'

--- src/task/_pathFinder.js
+++ src/task/_pathFinder.js
@@ -13,30 +13,32 @@ export default (creep, target) => {
                 let costs, room = Game.rooms[roomName];
                 if (!Memory.PathFinder) {
                     Memory.PathFinder = {}
-                    Memory.PathFinder[roomName] = []
                 }
-                costs = Memory.PathFinder[roomName]
+                if (Memory.PathFinder[roomName] && Memory.PathFinder.time && Game.time != Memory.PathFinder.time) {
+                    // In this example `room` will always exist, but since PathFinder
+                    // supports searches which span multiple rooms you should be careful!
+                    if (!room) return;
+                    costs = new PathFinder.CostMatrix;
+                    room.find(FIND_STRUCTURES).forEach((structure) => {
+                        if (structure.structureType === STRUCTURE_ROAD) {
+                            // Favor roads over plain tiles
+                            costs.set(structure.pos.x, structure.pos.y, 1);
+                        } else if (structure.structureType !== STRUCTURE_CONTAINER &&
+                            (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
+                            // Can't walk through non-walkable buildings
+                            costs.set(structure.pos.x, structure.pos.y, 0xff);
+                        }
+                    });
 
-                // In this example `room` will always exist, but since PathFinder
-                // supports searches which span multiple rooms you should be careful!
-                if (!room) return;
-                costs = new PathFinder.CostMatrix;
-                room.find(FIND_STRUCTURES).forEach((structure) => {
-                    if (structure.structureType === STRUCTURE_ROAD) {
-                        // Favor roads over plain tiles
-                        costs.set(structure.pos.x, structure.pos.y, 1);
-                    } else if (structure.structureType !== STRUCTURE_CONTAINER &&
-                        (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
-                        // Can't walk through non-walkable buildings
-                        costs.set(structure.pos.x, structure.pos.y, 0xff);
-                    }
-                });
-
-                // Avoid creeps in the room
-                room.find(FIND_CREEPS).forEach((creep) => {
-                    costs.set(creep.pos.x, creep.pos.y, 0xff);
-                });
+                    // Avoid creeps in the room
+                    room.find(FIND_CREEPS).forEach((creep) => {
+                        costs.set(creep.pos.x, creep.pos.y, 0xff);
+                    });
+                } else {
+                    costs = Memory.PathFinder[roomName]
+                }
                 Memory.PathFinder[roomName] = costs;
+                Memory.PathFinder.time = Game.time
                 return costs;
             }
         }).path;
