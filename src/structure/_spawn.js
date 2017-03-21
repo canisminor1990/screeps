baf3b0e575a408de6e76ab210d7f6e39c7d3cd48
canisminor1990 <canisminor1990@gmail.com>
2017-03-22 07:49:12 +0800

    updata

--- src/structure/_spawn.js
+++ src/structure/_spawn.js
@@ -3,61 +3,6 @@ const factory = config.role;
 
 export default (spawn) => {
 
-	const targetsBuild = spawn.room.memory.constructionSites;
-
-	for (let name in Memory.creeps) {
-		if (!Game.creeps[name]) {
-			delete Memory.creeps[name];
-			console.log(['[Clean]',
-			             name].join(' '));
-		}
-		// if (Memory.creeps[name].role = "builder" && targetsBuild.length == 0) {
-		// 	const builderTargets = spawn.pos.findInRange(FIND_MY_CREEPS, 1, {filter: creep => creep.role == "builder"});
-		// 	if (builderTargets.length > 0) {
-		// 		spawn.recycleCreep(builderTargets[0]);
-		// 	}
-		// }
-	}
-
-	for (let name in factory) {
-
-		const role = factory[name].role,
-				body = buildBody(factory[name].body),
-				number = factory[name].number,
-				numberSum = _.sum(number);
-
-		for (let i in number) {
-			const nowNumber = _.filter(Game.creeps, (creep) =>
-					creep.memory.role == role &&
-					creep.memory.source == i
-			).length;
-
-			if (number[i] > nowNumber) {
-
-				if (Game.spawns['Spawn1'].canCreateCreep(body) === OK) {
-					const name = `${role}#${getNowFormatDate()}`
-					Game.spawns['Spawn1'].createCreep(body, name, {role: role, source: i}
-					);
-					console.log(['[Spawn]',
-					             name,
-					             'Source:',
-					             i].join(' '));
-					//run
-					if (spawn.spawning) {
-						const spawningCreep = Game.creeps[spawn.spawning.name];
-						spawn.room.visual.text(
-								'[Spawn] ' + spawningCreep.memory.role,
-								spawn.pos.x + 1,
-								spawn.pos.y,
-								{align: 'left', opacity: 0.8});
-					}
-					return
-				} else {
-					return
-				}
-			}
-		}
-	}
 }
 
 function getNowFormatDate() {
