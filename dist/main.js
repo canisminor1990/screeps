3b3ae2d2f394ba653ed95655efb6333d56926b50
canisminor1990 <canisminor1990@gmail.com>
2017-03-21 22:22:36 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -1290,22 +1290,43 @@ var opt = {
 };
 
 exports.default = function (creep, target) {
-    var Pos = creep.pos;
-    var targetPos = target.pos;
-    var Path = void 0;
 
-    if (!(creep.memory.path && creep.memory.path.length > 0 || target !== creep.memory.target)) {
-        Path = PathFinder.search(Pos, targetPos, opt).path;
-    }
+    var goals = target.pos;
+    var ret = PathFinder.search(creep.pos, goals, {
+        // We need to set the defaults costs higher so that we
+        // can set the road cost lower in `roomCallback`
+        plainCost: 2,
+        swampCost: 10,
 
-    if (creep.move(Pos.getDirectionTo(Path.shift())) == 0) {
-        creep.memory.path = Path;
-        delete creep.memory.lastPos;
-    } else {
-        delete creep.memory.path;
-    }
+        roomCallback: function roomCallback(roomName) {
 
-    creep.memory.target = target;
+            var room = Game.rooms[roomName];
+            // In this example `room` will always exist, but since PathFinder
+            // supports searches which span multiple rooms you should be careful!
+            if (!room) return;
+            var costs = new PathFinder.CostMatrix();
+
+            room.find(FIND_STRUCTURES).forEach(function (structure) {
+                if (structure.structureType === STRUCTURE_ROAD) {
+                    // Favor roads over plain tiles
+                    costs.set(structure.pos.x, structure.pos.y, 1);
+                } else if (structure.structureType !== STRUCTURE_CONTAINER && (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
+                    // Can't walk through non-walkable buildings
+                    costs.set(structure.pos.x, structure.pos.y, 0xff);
+                }
+            });
+
+            // Avoid creeps in the room
+            room.find(FIND_CREEPS).forEach(function (creep) {
+                costs.set(creep.pos.x, creep.pos.y, 0xff);
+            });
+
+            return costs;
+        }
+    });
+
+    var pos = ret.path[0];
+    creep.move(creep.pos.getDirectionTo(pos));
 };
 
 function hasRoad(pos) {
--- src/task/_pathFinder.js
+++ src/task/_pathFinder.js
@@ -32,22 +32,48 @@ const opt = {
 }
 
 export default (creep, target) => {
-    const Pos = creep.pos;
-    const targetPos = target.pos
-    let Path;
 
-    if (!(creep.memory.path && creep.memory.path.length > 0 || target !== creep.memory.target)) {
-        Path = PathFinder.search(Pos, targetPos, opt).path;
-    }
+    let goals = target.pos
+    let ret = PathFinder.search(
+        creep.pos, goals,
+        {
+            // We need to set the defaults costs higher so that we
+            // can set the road cost lower in `roomCallback`
+            plainCost: 2,
+            swampCost: 10,
 
-    if (creep.move(Pos.getDirectionTo(Path.shift())) == 0) {
-        creep.memory.path = Path;
-        delete(creep.memory.lastPos);
-    } else {
-        delete(creep.memory.path);
-    }
+            roomCallback: function(roomName) {
 
-    creep.memory.target = target;
+                let room = Game.rooms[roomName];
+                // In this example `room` will always exist, but since PathFinder
+                // supports searches which span multiple rooms you should be careful!
+                if (!room) return;
+                let costs = new PathFinder.CostMatrix;
+
+                room.find(FIND_STRUCTURES).forEach(function(structure) {
+                    if (structure.structureType === STRUCTURE_ROAD) {
+                        // Favor roads over plain tiles
+                        costs.set(structure.pos.x, structure.pos.y, 1);
+                    } else if (structure.structureType !== STRUCTURE_CONTAINER &&
+                        (structure.structureType !== STRUCTURE_RAMPART ||
+                        !structure.my)) {
+                        // Can't walk through non-walkable buildings
+                        costs.set(structure.pos.x, structure.pos.y, 0xff);
+                    }
+                });
+
+                // Avoid creeps in the room
+                room.find(FIND_CREEPS).forEach(function(creep) {
+                    costs.set(creep.pos.x, creep.pos.y, 0xff);
+                });
+
+                return costs;
+            },
+        }
+    );
+
+    let pos = ret.path[0];
+    creep.move(creep.pos.getDirectionTo(pos));
 }
 
 function hasRoad(pos) {
