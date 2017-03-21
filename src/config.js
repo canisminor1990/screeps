56c4a0bb0de0b28d38dda9dc0bc61bae91c8a1f4
canisminor1990 <canisminor1990@gmail.com>
2017-03-21 17:32:28 +0800

    updata

--- src/config.js
+++ src/config.js
@@ -1,22 +1,19 @@
 const role = {
     number: {
-        claim: [1],
-        farMiner: [1],
-        farHarvester: [4],
+
         harvester: [0, 2],
-        upgrader: [2],
-        builder: [0, 2],
+
         miner: [1, 2],
-        cleaner: [1],
+
     },
     body: {//300 + 5 * 5 = 550
         claim: {move: 1, claim: 1},
         farMiner: {move: 3, work: 4, carry: 3}, // 3
         farHarvester: {move: 4, work: 1, carry: 4}, // 350
-        harvester: {move: 3, work: 1, carry: 6}, // 350
+        harvester: {move: 1, work: 1, carry: 2}, // 350
         upgrader: {move: 1, work: 4, carry: 2}, // 350
         builder: {move: 3, work: 3, carry: 3}, // 350
-        miner: {move: 2, work: 5, carry: 1}, // 3
+        miner: {move: 1, work: 2, carry: 1}, // 3
         cleaner: {move: 2, work: 1, carry: 2}, // 350
     }
 }
