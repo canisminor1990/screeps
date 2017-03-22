64d1d9fa2c62c7ae5bc75820a6ba203ce4d2443f
canisminor1990 <canisminor1990@gmail.com>
2017-03-22 08:29:37 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -1300,9 +1300,64 @@ exports.default = function (creep) {
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+var costs = new PathFinder.CostMatrix();
 
 exports.default = function (creep, target) {
-	creep.moveTo(target);
+	// const targetsBuild = creep.room.memory.constructionSites;
+	if (creep.fatigue > 1) return;
+	var goals = target.pos;
+	var path = void 0;
+	if (!creep.memory.path || !creep.memory.target || creep.memory.target != target.id) {
+		path = PathFinder.search(creep.pos, goals, {
+
+			plainCost: 5,
+			swampCost: 25,
+
+			roomCallback: function roomCallback(roomName) {
+				var room = Game.rooms[roomName];
+				if (!room) return;
+				if (!Memory.PathFinder) {
+					Memory.PathFinder = {};
+				}
+				costs = PathFinder.CostMatrix.deserialize(Memory.PathFinder[roomName]);
+				var timeout = 10; //(targetsBuild.length > 0) ? 10 : 100000
+				if (!Memory.PathFinder[roomName] || Game.time - Memory.PathFinder.time > timeout) {
+					// In this example `room` will always exist, but since PathFinder
+					// supports searches which span multiple rooms you should be careful!
+					room.find(FIND_STRUCTURES).forEach(function (structure) {
+						if (structure.structureType === STRUCTURE_ROAD) {
+							// Favor roads over plain tiles
+							costs.set(structure.pos.x, structure.pos.y, 1);
+						} else if (structure.structureType !== STRUCTURE_CONTAINER && (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
+							// Can't walk through non-walkable buildings
+							costs.set(structure.pos.x, structure.pos.y, 0xff);
+						}
+					});
+					// Avoid creeps in the room
+				}
+
+				if (Game.time != Memory.PathFinder.time) {
+					room.find(FIND_CREEPS).forEach(function (creep) {
+						costs.set(creep.pos.x, creep.pos.y, 0xff);
+					});
+					Memory.PathFinder[roomName] = costs.serialize();
+					Memory.PathFinder.time = Game.time;
+				} else {}
+
+				return costs;
+			}
+		}).path;
+	} else {
+		path = creep.memory.path;
+	}
+
+	if (creep.move(creep.pos.getDirectionTo(path.shift())) == OK) {
+		creep.memory.path = path;
+		creep.memory.target = target.id;
+	} else {
+		delete creep.memory.path;
+		delete creep.memory.target;
+	}
 };
 
 /***/ }),
--- src/task/_pathFinder.js
+++ src/task/_pathFinder.js
@@ -1,5 +1,64 @@
+let costs = new PathFinder.CostMatrix;
 export default (creep, target) => {
-	creep.moveTo(target)
+	// const targetsBuild = creep.room.memory.constructionSites;
+	if (creep.fatigue > 1)return;
+	let goals = target.pos
+	let path;
+	if (!creep.memory.path || !creep.memory.target || creep.memory.target != target.id) {
+		path = PathFinder.search(creep.pos, goals, {
+
+			plainCost: 5,
+			swampCost: 25,
+
+			roomCallback: (roomName) => {
+				let room = Game.rooms[roomName];
+				if (!room) return;
+				if (!Memory.PathFinder) {
+					Memory.PathFinder = {}
+				}
+				costs = PathFinder.CostMatrix.deserialize(Memory.PathFinder[roomName])
+				const timeout = 10 //(targetsBuild.length > 0) ? 10 : 100000
+				if (!Memory.PathFinder[roomName] || Game.time - Memory.PathFinder.time > timeout) {
+					// In this example `room` will always exist, but since PathFinder
+					// supports searches which span multiple rooms you should be careful!
+					room.find(FIND_STRUCTURES).forEach((structure) => {
+						if (structure.structureType === STRUCTURE_ROAD) {
+							// Favor roads over plain tiles
+							costs.set(structure.pos.x, structure.pos.y, 1);
+						} else if (structure.structureType !== STRUCTURE_CONTAINER &&
+								(structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
+							// Can't walk through non-walkable buildings
+							costs.set(structure.pos.x, structure.pos.y, 0xff);
+						}
+					});
+					// Avoid creeps in the room
+				}
+
+				if (Game.time != Memory.PathFinder.time) {
+					room.find(FIND_CREEPS).forEach((creep) => {
+						costs.set(creep.pos.x, creep.pos.y, 0xff);
+					});
+					Memory.PathFinder[roomName] = costs.serialize();
+					Memory.PathFinder.time = Game.time
+				} else {
+
+				}
+
+
+				return costs;
+			}
+		}).path;
+	} else {
+		path = creep.memory.path
+	}
+
+	if (creep.move(creep.pos.getDirectionTo(path.shift())) == OK) {
+		creep.memory.path = path;
+		creep.memory.target = target.id;
+	} else {
+		delete (creep.memory.path)
+		delete (creep.memory.target)
+	}
 }
 
 
