1a3f383ff7fc539eb4440a9445cfe3c0712e312c
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 01:33:39 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -429,67 +429,67 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 
 exports.default = function () {
-    var room = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : Game.rooms['W81S67'];
+	var room = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : Game.rooms['W81S67'];
 
-    var needBuild = room.memory.structures.needBuild;
-    var friends = ["Ruo", "FanHua"];
-    var repair = {
-        percent: 0.5,
-        maxHits: 10000
-    };
-    var role = [{
-        role: "claim",
-        body: { move: 1, claim: 1 },
-        number: 0,
-        priority: 7
-    }, {
-        role: "farMiner",
-        body: { move: 3, work: 4, carry: 3 },
-        number: 0,
-        priority: 4
-    }, {
-        role: 'farHarvester',
-        body: { move: 4, work: 0, carry: 4 },
-        number: 0,
-        priority: 5
-    }, {
-        role: 'harvester',
-        body: { move: 4, work: 0, carry: 8 },
-        number: 6,
-        priority: 2
-    }, {
-        role: 'upgrader',
-        body: { move: 1, work: 4, carry: 2 },
-        number: 2,
-        priority: 3
-    }, {
-        role: 'builder',
-        body: { move: 3, work: 3, carry: 3 },
-        number: needBuild.length > 0 ? 1 : 0,
-        priority: 6
-    }, {
-        role: "miner",
-        body: { move: 3, work: 8, carry: 1 },
-        number: 2,
-        priority: 1
-    }, {
-        role: 'cleaner',
-        body: { move: 2, work: 1, carry: 2 },
-        number: 1,
-        priority: 0
-    }];
+	var needBuild = room.memory.structures.needBuild;
+	var friends = ["Ruo", "FanHua"];
+	var repair = {
+		percent: 0.5,
+		maxHits: 10000
+	};
+	var role = [{
+		role: "claim",
+		body: { move: 1, claim: 1 },
+		number: 0,
+		priority: 7
+	}, {
+		role: "farMiner",
+		body: { move: 3, work: 4, carry: 3 },
+		number: 1,
+		priority: 4
+	}, {
+		role: 'farHarvester',
+		body: { move: 4, attack: 2, carry: 4, tough: 2 },
+		number: 1,
+		priority: 5
+	}, {
+		role: 'harvester',
+		body: { move: 4, work: 0, carry: 8 },
+		number: 6,
+		priority: 2
+	}, {
+		role: 'upgrader',
+		body: { move: 1, work: 4, carry: 2 },
+		number: 2,
+		priority: 3
+	}, {
+		role: 'builder',
+		body: { move: 3, work: 3, carry: 3 },
+		number: needBuild.length > 0 ? 1 : 0,
+		priority: 6
+	}, {
+		role: "miner",
+		body: { move: 3, work: 8, carry: 1 },
+		number: 2,
+		priority: 1
+	}, {
+		role: 'cleaner',
+		body: { move: 2, work: 1, carry: 2 },
+		number: 1,
+		priority: 0
+	}];
 
-    return {
-        role: role.sort(function (a, b) {
-            return a.priority - b.priority;
-        }),
-        friends: friends,
-        repair: repair
-    };
+	return {
+		role: role.sort(function (a, b) {
+			return a.priority - b.priority;
+		}),
+		friends: friends,
+		repair: repair
+	};
 };
 
 /***/ }),
--- src/config.js
+++ src/config.js
@@ -1,64 +1,65 @@
 export default (room = Game.rooms['W81S67']) => {
-    const needBuild = room.memory.structures.needBuild;
-    const friends   = ["Ruo", "FanHua"]
-    const repair    = {
-        percent: 0.5,
-        maxHits: 10000,
-    }
-    const role      = [
-        {
-            role    : "claim",
-            body    : {move: 1, claim: 1},
-            number  : 0,
-            priority: 7
-        },
-        {
-            role    : "farMiner",
-            body    : {move: 3, work: 4, carry: 3},
-            number  : 0,
-            priority: 4
-        },
-        {
-            role    : 'farHarvester',
-            body    : {move: 4, work: 0, carry: 4},
-            number  : 0,
-            priority: 5
-        },
-        {
-            role    : 'harvester',
-            body    : {move: 4, work: 0, carry: 8},
-            number  : 6,
-            priority: 2
-        },
-        {
-            role    : 'upgrader',
-            body    : {move: 1, work: 4, carry: 2},
-            number  : 2,
-            priority: 3
-        },
-        {
-            role    : 'builder',
-            body    : {move: 3, work: 3, carry: 3},
-            number  : (needBuild.length > 0) ? 1 : 0,
-            priority: 6
-        },
-        {
-            role    : "miner",
-            body    : {move: 3, work: 8, carry: 1},
-            number  : 2,
-            priority: 1
-        },
-        {
-            role    : 'cleaner',
-            body    : {move: 2, work: 1, carry: 2},
-            number  : 1,
-            priority: 0
-        }
-    ]
+	const needBuild = room.memory.structures.needBuild;
+	const friends = ["Ruo",
+	                 "FanHua"]
+	const repair = {
+		percent: 0.5,
+		maxHits: 10000,
+	}
+	const role = [
+		{
+			role: "claim",
+			body: {move: 1, claim: 1},
+			number: 0,
+			priority: 7
+		},
+		{
+			role: "farMiner",
+			body: {move: 3, work: 4, carry: 3},
+			number: 1,
+			priority: 4
+		},
+		{
+			role: 'farHarvester',
+			body: {move: 4, attack: 2, carry: 4, tough: 2},
+			number: 1,
+			priority: 5
+		},
+		{
+			role: 'harvester',
+			body: {move: 4, work: 0, carry: 8},
+			number: 6,
+			priority: 2
+		},
+		{
+			role: 'upgrader',
+			body: {move: 1, work: 4, carry: 2},
+			number: 2,
+			priority: 3
+		},
+		{
+			role: 'builder',
+			body: {move: 3, work: 3, carry: 3},
+			number: (needBuild.length > 0) ? 1 : 0,
+			priority: 6
+		},
+		{
+			role: "miner",
+			body: {move: 3, work: 8, carry: 1},
+			number: 2,
+			priority: 1
+		},
+		{
+			role: 'cleaner',
+			body: {move: 2, work: 1, carry: 2},
+			number: 1,
+			priority: 0
+		}
+	]
 
-    return {
-        role   : role.sort((a, b) => a.priority - b.priority),
-        friends: friends,
-        repair : repair,
-    };
+	return {
+		role: role.sort((a, b) => a.priority - b.priority),
+		friends: friends,
+		repair: repair,
+	};
 }
