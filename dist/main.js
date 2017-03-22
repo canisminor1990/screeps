44148c4c84ea4e8f941ce313fd693a86ef99d9ce
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 01:29:08 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -767,25 +767,29 @@ exports.default = function (creep, needBuild, newRoom) {
 		creep.memory.canBuild = true;
 	}
 
-	if (needBuild.length > 0) {
-		if (creep.memory.canBuild) {
-			var buildTarget = creep.pos.findClosestByRange(needBuild);
-			buildTarget && creep.build(buildTarget) != OK ? (0, _task.pathFinder)(creep, buildTarget) : null;
-		} else {
-			var canWithdraw = creep.pos.findClosestByRange(creep.room.memory.structures.canWithdraw);
-			canWithdraw && creep.withdraw(canWithdraw, RESOURCE_ENERGY) != OK ? (0, _task.pathFinder)(creep, canWithdraw) : null;
-		}
+	var newNeedBuild = newRoom.memory.structures.needBuild;
+	if (creep.memory.canBuild && newNeedBuild.length > 0) {
+		var newBuildTarget = Game.getObjectById(newNeedBuild[0].id);
+		(0, _task.pathFinder)(creep, newBuildTarget);
+		creep.build(newBuildTarget);
 	} else {
-		var newNeedBuild = newRoom.memory.structures.needBuild;
-		if (creep.memory.canBuild && newNeedBuild.length > 0) {
-			var newBuildTarget = Game.getObjectById(newNeedBuild[0].id);
-			(0, _task.pathFinder)(creep, newBuildTarget);
-			creep.build(newBuildTarget);
-		} else {
-			var storage = Game.getObjectById('58d07b35bfeec6256575be5d');
-			creep.withdraw(storage, RESOURCE_ENERGY) != OK ? (0, _task.pathFinder)(creep, storage) : null;
-		}
+		var storage = Game.getObjectById('58d07b35bfeec6256575be5d');
+		creep.withdraw(storage, RESOURCE_ENERGY) != OK ? (0, _task.pathFinder)(creep, storage) : null;
 	}
+	// if (needBuild.length > 0) {
+	// 	if (creep.memory.canBuild) {
+	// 		const buildTarget = creep.pos.findClosestByRange(needBuild);
+	// 		(buildTarget && creep.build(buildTarget) != OK)
+	// 				? pathFinder(creep, buildTarget) : null;
+	// 	} else {
+	// 		const canWithdraw = creep.pos.findClosestByRange(creep.room.memory.structures.canWithdraw);
+	// 		(canWithdraw && creep.withdraw(canWithdraw, RESOURCE_ENERGY) != OK)
+	// 				? pathFinder(creep, canWithdraw) : null
+	// 	}
+	// } else {
+	//
+	//
+	// }
 };
 
 /***/ }),
--- src/role/_builder.js
+++ src/role/_builder.js
@@ -10,28 +10,30 @@ export default (creep, needBuild, newRoom) => {
 		creep.memory.canBuild = true;
 	}
 
-	if (needBuild.length > 0) {
-		if (creep.memory.canBuild) {
-			const buildTarget = creep.pos.findClosestByRange(needBuild);
-			(buildTarget && creep.build(buildTarget) != OK)
-					? pathFinder(creep, buildTarget) : null;
-		} else {
-			const canWithdraw = creep.pos.findClosestByRange(creep.room.memory.structures.canWithdraw);
-			(canWithdraw && creep.withdraw(canWithdraw, RESOURCE_ENERGY) != OK)
-					? pathFinder(creep, canWithdraw) : null
-		}
+
+	let newNeedBuild = newRoom.memory.structures.needBuild;
+	if (creep.memory.canBuild && newNeedBuild.length > 0) {
+		const newBuildTarget = Game.getObjectById(newNeedBuild[0].id);
+		pathFinder(creep, newBuildTarget);
+		creep.build(newBuildTarget);
+
 	} else {
-		let newNeedBuild = newRoom.memory.structures.needBuild;
-		if (creep.memory.canBuild && newNeedBuild.length > 0) {
-			const newBuildTarget = Game.getObjectById(newNeedBuild[0].id);
-			pathFinder(creep, newBuildTarget);
-			creep.build(newBuildTarget);
-
-		} else {
-			let storage = Game.getObjectById('58d07b35bfeec6256575be5d');
-			(creep.withdraw(storage, RESOURCE_ENERGY) != OK)
-					? pathFinder(creep, storage) : null
-		}
-
+		let storage = Game.getObjectById('58d07b35bfeec6256575be5d');
+		(creep.withdraw(storage, RESOURCE_ENERGY) != OK)
+				? pathFinder(creep, storage) : null
 	}
+	// if (needBuild.length > 0) {
+	// 	if (creep.memory.canBuild) {
+	// 		const buildTarget = creep.pos.findClosestByRange(needBuild);
+	// 		(buildTarget && creep.build(buildTarget) != OK)
+	// 				? pathFinder(creep, buildTarget) : null;
+	// 	} else {
+	// 		const canWithdraw = creep.pos.findClosestByRange(creep.room.memory.structures.canWithdraw);
+	// 		(canWithdraw && creep.withdraw(canWithdraw, RESOURCE_ENERGY) != OK)
+	// 				? pathFinder(creep, canWithdraw) : null
+	// 	}
+	// } else {
+	//
+	//
+	// }
 }
