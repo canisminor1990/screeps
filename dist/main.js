ec8ec0ac6ea6de814dc89d0fa52f9f1855919749
canisminor1990 <canisminor1990@gmail.com>
2017-03-22 12:19:44 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -1259,73 +1259,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (creep, target) {
-	if (creep.fatigue != 0) {
-		return;
-	}
-	if (creep.memory.lastPos && creep.memory.lastPos == creep.pos) {
-		creep.moveTo(target);
-		return;
-	}
-
-	var goals = target.pos;
-	var path = void 0;
-	if (!creep.memory.path || !creep.memory.target || creep.memory.target != target.id) {
-		path = PathFinder.search(creep.pos, goals, {
-			maxOps: 1000,
-			plainCost: 5,
-			swampCost: 25,
-
-			roomCallback: function roomCallback(roomName) {
-				var costs = void 0,
-				    room = Game.rooms[roomName];
-				if (!room) {
-					return;
-				}
-				if (!Memory.PathFinder) {
-					Memory.PathFinder = {};
-				}
-
-				if (!Memory.PathFinder[roomName] || Game.time != Memory.PathFinder.time) {
-					costs = new PathFinder.CostMatrix();
-					// In this example `room` will always exist, but since PathFinder
-					// supports searches which span multiple rooms you should be careful!
-					room.find(FIND_STRUCTURES).forEach(function (structure) {
-						if (structure.structureType === STRUCTURE_ROAD) {
-							// Favor roads over plain tiles
-							costs.set(structure.pos.x, structure.pos.y, 1);
-						} else if (structure.structureType !== STRUCTURE_CONTAINER && (structure.structureType !== STRUCTURE_RAMPART || !structure.my)) {
-							// Can't walk through non-walkable buildings
-							costs.set(structure.pos.x, structure.pos.y, 0xff);
-						}
-					});
-					// Avoid creeps in the room
-					room.find(FIND_CREEPS).forEach(function (creep) {
-						costs.set(creep.pos.x, creep.pos.y, 0xff);
-					});
-				} else {
-					costs = PathFinder.CostMatrix.deserialize(Memory.PathFinder[roomName]);
-				}
-
-				Memory.PathFinder[roomName] = costs.serialize();
-				Memory.PathFinder.time = Game.time;
-
-				return costs;
-			}
-		});
-		console.log(path.ops);
-		path = path.path;
-	} else {
-		path = creep.memory.path;
-	}
-
-	if (creep.move(creep.pos.getDirectionTo(path.shift())) == OK) {
-		creep.memory.path = path;
-		creep.memory.target = target.id;
-	} else {
-		delete creep.memory.path;
-		delete creep.memory.target;
-	}
-	creep.memory.lastPos = creep.pos;
+	creep.moveTo(target);
 };
 
 /***/ }),
