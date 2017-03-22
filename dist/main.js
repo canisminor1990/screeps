8afa70a0799104f5ab3ee9cf579b1f59fb5de4e1
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 00:04:52 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -817,33 +817,31 @@ exports.default = function (creep) {
   var dropped = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
 
   var test = Game.getObjectById('58d07b35bfeec6256575be5d');
-  creep.transfer(test, RESOURCE_GHODIUM_OXIDE) != OK ? (0, _task.pathFinder)(creep, test) : null;
+  // (creep.transfer(test, RESOURCE_GHODIUM_OXIDE) != OK)
+  //     ? pathFinder(creep, test) : null
 
-  //
-  // if (creep.carry.energy < creep.carryCapacity) {
-  // 	if (dropped.length > 0) {
-  // 		const pickupTarget = creep.pos.findClosestByPath(dropped);
-  // 		(pickupTarget && creep.pickup(pickupTarget) != OK)
-  // 			? pathFinder(creep, pickupTarget) : null;
-  // 	} else {
-  //        const transferTarget = creep.room.memory.structures.container.sort((a, b) => b.store.enengy - a.store.enengy);
-  //        (transferTarget && creep.withdraw(transferTarget[0], RESOURCE_ENERGY) != OK)
-  //            ? pathFinder(creep, transferTarget[0]) : null
-  // 	}
-  // } else {
-  //
-  //    const needFill = creep.room.memory.structures.needFill;
-  //    let needFillTarget;
-  //    if (needFill.length > 0) {
-  //        needFillTarget = creep.pos.findClosestByRange(needFill);
-  //    } else {
-  //        needFillTarget = creep.room.storage
-  //    }
-  //    (needFillTarget && creep.transfer(needFillTarget, RESOURCE_ENERGY) != OK)
-  //        ? pathFinder(creep, needFillTarget) : null
-  //
-  //
-  // }
+
+  if (creep.carry.energy < creep.carryCapacity) {
+    if (dropped.length > 0) {
+      var pickupTarget = creep.pos.findClosestByPath(dropped);
+      pickupTarget && creep.pickup(pickupTarget) != OK ? (0, _task.pathFinder)(creep, pickupTarget) : null;
+    } else {
+      var transferTarget = creep.room.memory.structures.container.sort(function (a, b) {
+        return b.store.enengy - a.store.enengy;
+      });
+      transferTarget && creep.withdraw(transferTarget[0], RESOURCE_ENERGY) != OK ? (0, _task.pathFinder)(creep, transferTarget[0]) : null;
+    }
+  } else {
+
+    var needFill = creep.room.memory.structures.needFill;
+    var needFillTarget = void 0;
+    if (needFill.length > 0) {
+      needFillTarget = creep.pos.findClosestByRange(needFill);
+    } else {
+      needFillTarget = creep.room.storage;
+    }
+    needFillTarget && creep.transfer(needFillTarget, RESOURCE_ENERGY) != OK ? (0, _task.pathFinder)(creep, needFillTarget) : null;
+  }
 };
 
 /***/ }),
--- src/role/_cleaner.js
+++ src/role/_cleaner.js
@@ -2,32 +2,32 @@ import { pathFinder } from '../task'
 
 export default (creep, dropped = []) => {
  const test = Game.getObjectById('58d07b35bfeec6256575be5d');
-    (creep.transfer(test, RESOURCE_GHODIUM_OXIDE) != OK)
-        ? pathFinder(creep, test) : null
+    // (creep.transfer(test, RESOURCE_GHODIUM_OXIDE) != OK)
+    //     ? pathFinder(creep, test) : null
 
-    //
-	// if (creep.carry.energy < creep.carryCapacity) {
-	// 	if (dropped.length > 0) {
-	// 		const pickupTarget = creep.pos.findClosestByPath(dropped);
-	// 		(pickupTarget && creep.pickup(pickupTarget) != OK)
-	// 			? pathFinder(creep, pickupTarget) : null;
-	// 	} else {
-     //        const transferTarget = creep.room.memory.structures.container.sort((a, b) => b.store.enengy - a.store.enengy);
-     //        (transferTarget && creep.withdraw(transferTarget[0], RESOURCE_ENERGY) != OK)
-     //            ? pathFinder(creep, transferTarget[0]) : null
-	// 	}
-	// } else {
-    //
-     //    const needFill = creep.room.memory.structures.needFill;
-     //    let needFillTarget;
-     //    if (needFill.length > 0) {
-     //        needFillTarget = creep.pos.findClosestByRange(needFill);
-     //    } else {
-     //        needFillTarget = creep.room.storage
-     //    }
-     //    (needFillTarget && creep.transfer(needFillTarget, RESOURCE_ENERGY) != OK)
-     //        ? pathFinder(creep, needFillTarget) : null
-    //
-    //
-	// }
+
+	if (creep.carry.energy < creep.carryCapacity) {
+		if (dropped.length > 0) {
+			const pickupTarget = creep.pos.findClosestByPath(dropped);
+			(pickupTarget && creep.pickup(pickupTarget) != OK)
+				? pathFinder(creep, pickupTarget) : null;
+		} else {
+            const transferTarget = creep.room.memory.structures.container.sort((a, b) => b.store.enengy - a.store.enengy);
+            (transferTarget && creep.withdraw(transferTarget[0], RESOURCE_ENERGY) != OK)
+                ? pathFinder(creep, transferTarget[0]) : null
+		}
+	} else {
+
+        const needFill = creep.room.memory.structures.needFill;
+        let needFillTarget;
+        if (needFill.length > 0) {
+            needFillTarget = creep.pos.findClosestByRange(needFill);
+        } else {
+            needFillTarget = creep.room.storage
+        }
+        (needFillTarget && creep.transfer(needFillTarget, RESOURCE_ENERGY) != OK)
+            ? pathFinder(creep, needFillTarget) : null
+
+
+	}
 };
