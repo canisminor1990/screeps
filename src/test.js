1a5776c44338c23172c8133556f260188738db79
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 11:33:33 +0800

    updata

--- src/test.js
+++ src/test.js
@@ -1,5 +1,9 @@
-export default (creep, target,color = '#ffffff') => {
+export default (creep, target, color = '#ffffff') => {
 	if (creep.fatigue > 0) return;
-	creep.moveTo(target, {reusePath: 12, visualizePathStyle: {stroke: color}})
+	creep.moveTo(target, {
+		reusePath         : 12,
+		serializeMemory   : true,
+		visualizePathStyle: {stroke: color}
+	})
 }
 
