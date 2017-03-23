eabcd0191e8fe87f9a7adbe8093844da03f3e882
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 22:41:29 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -1131,54 +1131,54 @@ exports.default = function (room, miner) {
 
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+		value: true
 });
 
 exports.default = function (room, config) {
 
-	var structures = room.find(FIND_STRUCTURES),
-	    structuresStorage = room.storage,
-	    structuresMy = _.filter(structures, function (structure) {
-		return structure.my;
-	}),
-	    structuresOther = _.filter(structures, function (structure) {
-		return !structure.my;
-	}),
-	    structuresContainer = _.filter(structuresOther, function (structure) {
-		return structure.structureType == STRUCTURE_CONTAINER;
-	});
+		var structures = room.find(FIND_STRUCTURES),
+		    structuresStorage = room.storage,
+		    structuresMy = _.filter(structures, function (structure) {
+				return structure.my;
+		}),
+		    structuresOther = _.filter(structures, function (structure) {
+				return !structure.my;
+		}),
+		    structuresContainer = _.filter(structuresOther, function (structure) {
+				return structure.structureType == STRUCTURE_CONTAINER;
+		});
 
-	var structuresDocker = _.compact(structuresContainer.concat([structuresStorage]));
+		var structuresDocker = _.compact(structuresContainer.concat([structuresStorage]));
 
-	return {
-		terminal: room.terminal,
-		controller: room.controller,
-		storage: structuresStorage,
-		tower: _.filter(structuresMy, function (structure) {
-			return structure.structureType == STRUCTURE_TOWER;
-		})[0],
-		spawn: _.filter(structuresMy, function (structure) {
-			return structure.structureType == STRUCTURE_SPAWN;
-		})[0],
-		container: _.filter(structuresOther, function (structure) {
-			return structure.structureType == STRUCTURE_CONTAINER;
-		}),
-		canWithdraw: structuresDocker.length > 0 ? _.filter(structuresDocker, function (structure) {
-			return structure.store.energy > 0;
-		}) : [],
-		canFill: structuresDocker.length > 0 ? _.filter(structuresDocker, function (structure) {
-			return structure.store.energy < structure.storeCapacity;
-		}) : [],
-		needFill: _.filter(structuresMy, function (structure) {
-			return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity && structure.energy < 300;
-		}),
-		needFix: _.filter(structures, function (structure) {
-			return (structure.my || structure.structureType == STRUCTURE_ROAD || structure.structureType == STRUCTURE_WALL) && structure.hits / structure.hitsMax < config.repair.percent && structure.hits < config.repair.maxHits;
-		}).sort(function (a, b) {
-			return a.hits - b.hits;
-		}),
-		needBuild: room.find(FIND_MY_CONSTRUCTION_SITES)
-	};
+		return {
+				terminal: room.terminal,
+				controller: room.controller,
+				storage: structuresStorage,
+				tower: _.filter(structuresMy, function (structure) {
+						return structure.structureType == STRUCTURE_TOWER;
+				})[0],
+				spawn: _.filter(structuresMy, function (structure) {
+						return structure.structureType == STRUCTURE_SPAWN;
+				})[0],
+				container: _.filter(structuresOther, function (structure) {
+						return structure.structureType == STRUCTURE_CONTAINER;
+				}),
+				canWithdraw: structuresDocker.length > 0 ? _.filter(structuresDocker, function (structure) {
+						return structure.store && structure.store.energy > 0;
+				}) : [],
+				canFill: structuresDocker.length > 0 ? _.filter(structuresDocker, function (structure) {
+						return structure.store && structure.store.energy < structure.storeCapacity;
+				}) : [],
+				needFill: _.filter(structuresMy, function (structure) {
+						return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity && structure.energy < 300;
+				}),
+				needFix: _.filter(structures, function (structure) {
+						return (structure.my || structure.structureType == STRUCTURE_ROAD || structure.structureType == STRUCTURE_WALL) && structure.hits / structure.hitsMax < config.repair.percent && structure.hits < config.repair.maxHits;
+				}).sort(function (a, b) {
+						return a.hits - b.hits;
+				}),
+				needBuild: room.find(FIND_MY_CONSTRUCTION_SITES)
+		};
 };
 
 /***/ }),
