4261f73027440788ac4569ff0a4ace5ce18b4591
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 12:56:29 +0800

    updata

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ export default (room = Game.rooms['W81S67']) => {
 	const role      = [
 		{
 			role    : "claim",
-			body    : {move: 1, claim: 1,tough: 1},
+			body    : {tough: 1, move: 1, claim: 1},
 			number  : 1,
 			priority: 7
 		},
@@ -19,37 +19,37 @@ export default (room = Game.rooms['W81S67']) => {
 		},
 		{
 			role    : 'farHarvester',
-			body    : {move: 3, attack: 1, carry: 4, tough: 1},
+			body    : {tough: 1, move: 3, carry: 4, attack: 1},
 			number  : 3,
 			priority: 5
 		},
 		{
 			role    : 'harvester',
-			body    : {move: 4, carry: 12},
+			body    : {carry: 12, move: 4},
 			number  : 4,
 			priority: 2
 		},
 		{
 			role    : 'upgrader',
-			body    : {move: 1, work: 4, carry: 2},
+			body    : {carry: 2, work: 4, move: 1},
 			number  : 2,
 			priority: 3
 		},
 		{
 			role    : 'builder',
-			body    : {move: 3, work: 3, carry: 3},
+			body    : {work: 3, carry: 3, move: 3},
 			number  : (needBuild.length > 0) ? 1 : 0,
 			priority: 6
 		},
 		{
 			role    : "miner",
-			body    : {move: 2, work: 8, carry: 1},
+			body    : {carry: 1, work: 8, move: 2},
 			number  : 2,
 			priority: 1
 		},
 		{
 			role    : 'cleaner',
-			body    : {move: 1, carry: 2},
+			body    : {carry: 2, move: 1},
 			number  : 2,
 			priority: 0
 		}
