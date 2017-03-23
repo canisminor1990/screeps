aeb73b5e6f4e9137450320ef463fe6369d67af3c
canisminor1990 <canisminor1990@gmail.com>
2017-03-23 08:17:16 +0800

    updata

--- src/manager/_structure.js
+++ src/manager/_structure.js
@@ -7,7 +7,7 @@ export default (room, roomNext) => {
 	const targetCreeps = Memory.creeps;
 	const config = Memory.config;
 
-	const spawn = (nextMemory.creeps.my)?_.merge(targetCreeps.my, nextMemory.creeps.my):targetCreeps.my;
+	const spawn = (nextMemory.creeps)?_.merge(targetCreeps.my, nextMemory.creeps.my):targetCreeps.my;
 
 	structure.spawn(targetStructures.spawn, spawn, config);
 	structure.tower(targetStructures.tower, targetStructures.needFix, targetCreeps.enemy);
