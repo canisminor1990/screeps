78f9222cff802442593e611206f330873243dab1
canisminor1990 <canisminor1990@gmail.com>
2017-03-24 23:34:39 +0800

    'updata'

--- src/role/_attacker.js
+++ src/role/_attacker.js
@@ -1,7 +1,7 @@
 import {attack, dismantle} from '../action'
 import {flags} from '../task'
 export default (creep) => {
-    let memoryFlags = creep.room.find(FIND_FLAGS).sort((a, b) => a.secondaryColor - b.secondaryColor).sort((a, b) => a.color - b.color)
+    let memoryFlags = creep.pos.findInRange(FIND_FLAGS).sort((a, b) => a.secondaryColor - b.secondaryColor).sort((a, b) => a.color - b.color)
 
     if (memoryFlags.length > 0){
         flags(creep,memoryFlags[0])
