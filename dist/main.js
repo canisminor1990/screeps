7a6db0acda8e22bbc715d78c35865e1bb3b9916b
canisminor1990 <canisminor1990@gmail.com>
2017-03-25 20:14:42 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2476,9 +2476,9 @@ exports.default = function (roomName) {
 	room.find(FIND_MY_CREEPS).forEach(function (creep) {
 		"use strict";
 
-		room.visual.rect(creep.pos.x, creep.pos.y - 1, 1, 0.2, { fill: '#000' });
+		room.visual.rect(creep.pos.x - 1, creep.pos.y - 1, 1, 0.2, { fill: '#000' });
 
-		room.visual.rect(creep.pos.x, creep.pos.y - 1, 1 / 1500 * creep.ticksToLive, 0.2, {
+		room.visual.rect(creep.pos.x - 1, creep.pos.y - 1, 1 / 1500 * creep.ticksToLive, 0.2, {
 			fill: '#66D9EF',
 			opacity: 1
 		});
--- src/task/_gui.js
+++ src/task/_gui.js
@@ -3,14 +3,14 @@ export default (roomName) => {
 	room.find(FIND_MY_CREEPS).forEach(creep => {
 		"use strict";
 		room.visual.rect(
-			creep.pos.x,
+			creep.pos.x-1,
 			creep.pos.y - 1,
 			1,
 			0.2,
 			{fill: '#000'});
 		
 		room.visual.rect(
-			creep.pos.x,
+			creep.pos.x-1,
 			creep.pos.y - 1,
 			1 / 1500 * creep.ticksToLive,
 			0.2,
