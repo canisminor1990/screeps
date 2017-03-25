6e3ab5b1012a7ea4f183dfa1553c868a6e365af1
canisminor1990 <canisminor1990@gmail.com>
2017-03-26 00:44:49 +0800

    'updata'

--- src/role/_miner.js
+++ src/role/_miner.js
@@ -15,9 +15,6 @@ export default (creep) => {
 		}
 	}
 	target        = Game.getObjectById(creep.memory.harvestTarget)
-	let container = target.pos.findInRange(creep.room.memory.structures.container, 1)
-	if (container.length > 0 && container[0].hits < container[0].hitsMax) {
-		if (repair(creep, container[0])) return;
-	}
+
 	if (harvest(creep, target)) return;
 }
