240ade77890bbc5d895c9861787c8e2effdfc7b0
canisminor1990 <canisminor1990@gmail.com>
2017-03-25 23:53:29 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2276,49 +2276,53 @@ exports.default = function (creep) {
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+	value: true
 });
 
 var _action = __webpack_require__(1);
 
 exports.default = function (creep) {
-    var memory = creep.room.memory;
-    var target = void 0;
-    // root
+	var memory = creep.room.memory;
+	var target = void 0;
+	// root
 
-    if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
-    target = Game.getObjectById(creep.memory.harvestTarget);
-    if ((0, _action.harvest)(creep, target)) {}
-    // if (!creep.memory.position) {
-    // 	target = creep.pos.findInRange(memory.structures.container, 0)
-    // 	if (target.length > 0) creep.memory.position = true;
-    // 	target = creep.pos.findClosestByRange(memory.structures.container)
-    // 	moveTo(creep, target)
-    // } else {
-    // 	return
-    // }
-    // return
+	if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
+	target = Game.getObjectById(creep.memory.harvestTarget);
+	var container = target.pos.findClosestByRange(target.room.memory.structures.container);
+	if (container && creep.carry.energy >= 50 && target.hits < target.hitsMax / 5) {
+		if (repair(creep, container)) return;
+	}
+	if ((0, _action.harvest)(creep, target)) {}
+	// if (!creep.memory.position) {
+	// 	target = creep.pos.findInRange(memory.structures.container, 0)
+	// 	if (target.length > 0) creep.memory.position = true;
+	// 	target = creep.pos.findClosestByRange(memory.structures.container)
+	// 	moveTo(creep, target)
+	// } else {
+	// 	return
+	// }
+	// return
 
 
-    // // memory
-    // isFull(creep)
-    // // run
-    // if (creep.memory.full) {
-    // 	target = creep.pos.findInRange(creep.room.memory.structures.canFill, 4);
-    // 	if (transfer(creep, target[0])) return;
-    // 	if (creep.memory.harvestTarget == "5873bc3511e3e4361b4d7393") {
-    // 		target = creep.room.controller;
-    // 		if (upgradeController(creep, target)) return;
-    // 	}
-    // } else {
-    // 	const dropped = creep.room.memory.dropped.energy;
-    // 	if (dropped.length > 0) {
-    // 		target = creep.pos.findInRange(dropped, 0);
-    // 		if (pickup(creep, target[0])) return;
-    // 	}
-    // 	target = Game.getObjectById(creep.memory.harvestTarget)
-    // 	if (harvest(creep, target)) return;
-    // }
+	// // memory
+	// isFull(creep)
+	// // run
+	// if (creep.memory.full) {
+	// 	target = creep.pos.findInRange(creep.room.memory.structures.canFill, 4);
+	// 	if (transfer(creep, target[0])) return;
+	// 	if (creep.memory.harvestTarget == "5873bc3511e3e4361b4d7393") {
+	// 		target = creep.room.controller;
+	// 		if (upgradeController(creep, target)) return;
+	// 	}
+	// } else {
+	// 	const dropped = creep.room.memory.dropped.energy;
+	// 	if (dropped.length > 0) {
+	// 		target = creep.pos.findInRange(dropped, 0);
+	// 		if (pickup(creep, target[0])) return;
+	// 	}
+	// 	target = Game.getObjectById(creep.memory.harvestTarget)
+	// 	if (harvest(creep, target)) return;
+	// }
 };
 
 /***/ }),
--- src/role/_miner.js
+++ src/role/_miner.js
@@ -1,40 +1,44 @@
 import {moveTo, harvest} from '../action'
 export default (creep) => {
-    const memory = creep.room.memory;
-    let target;
-    // root
-
-    if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
-    target = Game.getObjectById(creep.memory.harvestTarget)
-    if (harvest(creep, target)) {
-        // if (!creep.memory.position) {
-        // 	target = creep.pos.findInRange(memory.structures.container, 0)
-        // 	if (target.length > 0) creep.memory.position = true;
-        // 	target = creep.pos.findClosestByRange(memory.structures.container)
-        // 	moveTo(creep, target)
-        // } else {
-        // 	return
-        // }
-        // return
-    }
-
-    // // memory
-    // isFull(creep)
-    // // run
-    // if (creep.memory.full) {
-    // 	target = creep.pos.findInRange(creep.room.memory.structures.canFill, 4);
-    // 	if (transfer(creep, target[0])) return;
-    // 	if (creep.memory.harvestTarget == "5873bc3511e3e4361b4d7393") {
-    // 		target = creep.room.controller;
-    // 		if (upgradeController(creep, target)) return;
-    // 	}
-    // } else {
-    // 	const dropped = creep.room.memory.dropped.energy;
-    // 	if (dropped.length > 0) {
-    // 		target = creep.pos.findInRange(dropped, 0);
-    // 		if (pickup(creep, target[0])) return;
-    // 	}
-    // 	target = Game.getObjectById(creep.memory.harvestTarget)
-    // 	if (harvest(creep, target)) return;
-    // }
+	const memory = creep.room.memory;
+	let target;
+	// root
+	
+	if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
+	target        = Game.getObjectById(creep.memory.harvestTarget)
+	let container = target.pos.findClosestByRange(target.room.memory.structures.container)
+	if (container && creep.carry.energy >= 50 && target.hits < target.hitsMax / 5) {
+		if (repair(creep, container)) return;
+	}
+	if (harvest(creep, target)) {
+		// if (!creep.memory.position) {
+		// 	target = creep.pos.findInRange(memory.structures.container, 0)
+		// 	if (target.length > 0) creep.memory.position = true;
+		// 	target = creep.pos.findClosestByRange(memory.structures.container)
+		// 	moveTo(creep, target)
+		// } else {
+		// 	return
+		// }
+		// return
+	}
+	
+	// // memory
+	// isFull(creep)
+	// // run
+	// if (creep.memory.full) {
+	// 	target = creep.pos.findInRange(creep.room.memory.structures.canFill, 4);
+	// 	if (transfer(creep, target[0])) return;
+	// 	if (creep.memory.harvestTarget == "5873bc3511e3e4361b4d7393") {
+	// 		target = creep.room.controller;
+	// 		if (upgradeController(creep, target)) return;
+	// 	}
+	// } else {
+	// 	const dropped = creep.room.memory.dropped.energy;
+	// 	if (dropped.length > 0) {
+	// 		target = creep.pos.findInRange(dropped, 0);
+	// 		if (pickup(creep, target[0])) return;
+	// 	}
+	// 	target = Game.getObjectById(creep.memory.harvestTarget)
+	// 	if (harvest(creep, target)) return;
+	// }
 }
