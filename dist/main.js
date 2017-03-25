68e607c647c1f4527797cf6b7372f89c5a8eb361
canisminor1990 <canisminor1990@gmail.com>
2017-03-26 00:42:43 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2324,38 +2324,11 @@ exports.default = function (creep) {
 		}
 	}
 	target = Game.getObjectById(creep.memory.harvestTarget);
-
-	if ((0, _action.harvest)(creep, target)) {}
-	// if (!creep.memory.position) {
-	// 	target = creep.pos.findInRange(memory.structures.container, 0)
-	// 	if (target.length > 0) creep.memory.position = true;
-	// 	target = creep.pos.findClosestByRange(memory.structures.container)
-	// 	moveTo(creep, target)
-	// } else {
-	// 	return
-	// }
-	// return
-
-
-	// // memory
-	// isFull(creep)
-	// // run
-	// if (creep.memory.full) {
-	// 	target = creep.pos.findInRange(creep.room.memory.structures.canFill, 4);
-	// 	if (transfer(creep, target[0])) return;
-	// 	if (creep.memory.harvestTarget == "5873bc3511e3e4361b4d7393") {
-	// 		target = creep.room.controller;
-	// 		if (upgradeController(creep, target)) return;
-	// 	}
-	// } else {
-	// 	const dropped = creep.room.memory.dropped.energy;
-	// 	if (dropped.length > 0) {
-	// 		target = creep.pos.findInRange(dropped, 0);
-	// 		if (pickup(creep, target[0])) return;
-	// 	}
-	// 	target = Game.getObjectById(creep.memory.harvestTarget)
-	// 	if (harvest(creep, target)) return;
-	// }
+	var container = target.pos.findInRange(target.room.memory.structures.container, 1);
+	if (container.length > 0 && container[0].hits < container[0].hitsMax) {
+		if (repair(creep, container[0])) return;
+	}
+	if ((0, _action.harvest)(creep, target)) return;
 };
 
 /***/ }),
