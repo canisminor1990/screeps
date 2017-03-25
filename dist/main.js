c2aeb47651c09b5485e61a63195cefb36a0f470a
canisminor1990 <canisminor1990@gmail.com>
2017-03-25 21:27:07 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -1456,7 +1456,7 @@ exports.default = function (roomName) {
 };
 
 function gui(room, x, y, color, content) {
-	room.visual.rect(x, y + 0.3, 6, 0.7, { fill: '#fff', opacity: 0.2 }).rect(x, y + 0.3, 6 / content[2], 0.7, { fill: color, opacity: 0.7 }).text(content[0], x, y, { font: 0.5, align: 'left', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1 }).text(content[1], x + 6, y, { font: 0.4, align: 'right', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1 }).text(content[3], x + .2, y + 0.8, {
+	room.visual.rect(x, y + 0.3, 6, 0.7, { fill: '#fff', opacity: 0.2 }).rect(x, y + 0.3, 6 * content[2], 0.7, { fill: color, opacity: 0.7 }).text(content[0], x, y, { font: 0.5, align: 'left', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1 }).text(content[1], x + 6, y, { font: 0.4, align: 'right', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1 }).text(content[3], x + .2, y + 0.8, {
 		font: 0.4,
 		align: 'left',
 		stroke: 'rgba(0,0,0,.7)',
--- src/gui/_cl.js
+++ src/gui/_cl.js
@@ -38,7 +38,7 @@ export default (roomName) => {
 function gui(room, x, y, color, content) {
 	room.visual
 		.rect(x, y + 0.3, 6, 0.7, {fill: '#fff', opacity: 0.2})
-		.rect(x, y + 0.3, 6 / content[2], 0.7, {fill: color, opacity: 0.7})
+		.rect(x, y + 0.3, 6 * content[2], 0.7, {fill: color, opacity: 0.7})
 		.text(content[0], x, y, {font: 0.5, align: 'left', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1})
 		.text(content[1], x + 6, y, {font: 0.4, align: 'right', stroke: 'rgba(0,0,0,.7)', strokeWidth: 0.1})
 		.text(content[3], x + .2, y + 0.8, {
