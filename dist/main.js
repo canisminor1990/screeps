fd9b12d63780ec819237d5a84ca0e23ba80b0247
canisminor1990 <canisminor1990@gmail.com>
2017-03-25 14:09:25 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2393,7 +2393,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 var rooms = ['W81S67', 'W81S66', 'W82S67'];
 _screepsProfiler2.default.enable();
-
+console.log('# Coding Update!');
 module.exports.loop = function () {
 	if (Game.cpuLimit > 100) {
 		_screepsProfiler2.default.wrap(function () {
--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import * as Manager from './manager'
 import {timer} from  './_util'
 import {log} from  './task'
 import profiler from 'screeps-profiler';
-
-
 const rooms = ['W81S67', 'W81S66', 'W82S67'];
 profiler.enable();
-
-
+console.log('# Coding Update!')
 module.exports.loop = () => {
 	if (Game.cpuLimit > 100) {
 		profiler.wrap(() => {
