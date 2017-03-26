e9394b3eec172b59711828895619876595a262e1
canisminor1990 <canisminor1990@gmail.com>
2017-03-26 16:11:42 +0800

    'updata'

--- src/action/_moveTo.js
+++ src/action/_moveTo.js
@@ -1,20 +1,19 @@
 import {action} from "../_util"
 export default (creep, target, color = '#ffffff') => {
-
-
-    if (creep.fatigue > 0) return false;
-    if (!target) return false;
 	
-	if (target.pos && target.room) {
+	if (creep.fatigue > 0) return false;
+	if (!target) return false;
+	
+	if (target.pos && target.room && !Game.rooms[target.room.name]) {
 		target = new RoomPosition(target.pos.x, target.pos.y, target.room.name)
 	}
-
-    if (action(creep, target, creep.moveTo(target, {
-            reusePath: 12,
-            serializeMemory: true,
-            visualizePathStyle: {stroke: color}
-        }))) return true;
-
+	
+	if (action(creep, target, creep.moveTo(target, {
+			reusePath         : 12,
+			serializeMemory   : true,
+			visualizePathStyle: {stroke: color}
+		}))) return true;
+	
 }
 
 
