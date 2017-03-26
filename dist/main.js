28d212a70af1cf332aa1661c7390a8e96b868d54
canisminor1990 <canisminor1990@gmail.com>
2017-03-26 23:13:59 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -2797,19 +2797,19 @@ exports.default = function (creep) {
 	(0, _util.isFull)(creep);
 	//run
 	if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
-	target = Game.getObjectById(creep.memory.harvestTarget);
+	var harvestTarget = Game.getObjectById(creep.memory.harvestTarget);
 	if (creep.memory.full) {
-		var container = target.pos.findInRange(memory.structures.container, 1);
+		var container = harvestTarget.pos.findInRange(memory.structures.container, 1);
 		if (container && container.length > 0 && container[0].hits < container[0].hitsMax / 2) {
-			(0, _action.repair)(creep, container[0]);
+			if ((0, _action.repair)(creep, container[0])) return;
 		}
 		var needBuild = creep.room.memory.structures.needBuild;
 		if (needBuild.length > 0) {
 			target = creep.pos.findInRange(needBuild, 0);
-			if (target.length > 0) (0, _action.build)(creep, target[0]);
+			if (target.length > 0 && (0, _action.build)(creep, target[0])) return;
 		}
 	}
-	if ((0, _action.harvest)(creep, target)) return;
+	if ((0, _action.harvest)(creep, harvestTarget)) return;
 };
 
 /***/ }),
--- src/role/_miner.js
+++ src/role/_miner.js
@@ -7,17 +7,17 @@ export default (creep) => {
 	isFull(creep)
 	//run
 	if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
-	target = Game.getObjectById(creep.memory.harvestTarget)
+	const harvestTarget = Game.getObjectById(creep.memory.harvestTarget)
 	if (creep.memory.full) {
-		const container = target.pos.findInRange(memory.structures.container, 1)
+		const container = harvestTarget.pos.findInRange(memory.structures.container, 1)
 		if (container && container.length > 0 && container[0].hits < container[0].hitsMax / 2) {
-			repair(creep, container[0])
+			if (repair(creep, container[0])) return;
 		}
 		const needBuild = creep.room.memory.structures.needBuild;
 		if (needBuild.length > 0) {
 			target = creep.pos.findInRange(needBuild, 0);
-			if (target.length > 0) build(creep, target[0]);
+			if (target.length > 0 && build(creep, target[0]))return;
 		}
 	}
-	if (harvest(creep, target)) return;
+	if (harvest(creep, harvestTarget)) return;
 }
