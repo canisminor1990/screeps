28ccf75e17f8cd81eb91e54c6f43c2d1d309c6ea
canisminor1990 <canisminor1990@gmail.com>
2017-03-26 23:12:52 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -2797,18 +2797,18 @@ exports.default = function (creep) {
 	(0, _util.isFull)(creep);
 	//run
 	if (!creep.memory.harvestTarget) creep.memory.harvestTarget = memory.sources[0].source.id;
+	target = Game.getObjectById(creep.memory.harvestTarget);
 	if (creep.memory.full) {
 		var container = target.pos.findInRange(memory.structures.container, 1);
 		if (container && container.length > 0 && container[0].hits < container[0].hitsMax / 2) {
-			if ((0, _action.repair)(creep, container[0])) return;
+			(0, _action.repair)(creep, container[0]);
 		}
 		var needBuild = creep.room.memory.structures.needBuild;
 		if (needBuild.length > 0) {
 			target = creep.pos.findInRange(needBuild, 0);
-			if (target.length > 0 && (0, _action.build)(creep, target[0])) return;
+			if (target.length > 0) (0, _action.build)(creep, target[0]);
 		}
 	}
-	target = Game.getObjectById(creep.memory.harvestTarget);
 	if ((0, _action.harvest)(creep, target)) return;
 };
 
