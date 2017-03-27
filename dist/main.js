04016b8cabc4c6075fa78c41735fa4030a703773
canisminor1990 <canisminor1990@gmail.com>
2017-03-28 07:05:27 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -2375,11 +2375,11 @@ exports.default = function (room) {
 			return a.miner.length - b.miner.length;
 		});
 	}
-	if (sources.length > 1 && sources[0].miner.length == 0 && sources[sources.length - 1].miner.length > 1) {
-		var targetSource = sources[sources.length - 1],
-		    targetCreep = Game.getObjectById(targetSource.miner[targetSource.miner.length - 1].id);
-		(0, _util.targetChange)(targetCreep, sources[0].source, 'harvest');
-	}
+	// if (sources.length > 1 && sources[0].miner.length == 0 && sources[sources.length - 1].miner.length > 1) {
+	// 	const targetSource = sources[sources.length - 1],
+	// 	      targetCreep  = Game.getObjectById(targetSource.miner[targetSource.miner.length - 1].id);
+	// 	targetChange(targetCreep, sources[0].source, 'harvest')
+	// }
 	return sources;
 };
 
