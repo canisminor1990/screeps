c268232722a05a3cb756aecefe3bd8b56899dafc
canisminor1990 <canisminor1990@gmail.com>
2017-03-27 15:16:49 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -2974,7 +2974,7 @@ exports.default = function (creep) {
 		target = (0, _action.findClosestInRange)(creep, creep.room.memory.dropped.energy, 4);
 		if ((0, _action.pickup)(creep, target)) return;
 		target = Game.getObjectById('58d6a0f58f53422d7fea1d52');
-		if ((0, _action.withdraw)(creep, target, target.store.energy > 0)) return;
+		if ((0, _action.withdraw)(creep, target, RESOURCE_ENERGY, target.store.energy > 0)) return;
 	}
 };
 
