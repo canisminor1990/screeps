61b55b9025ffbc7fffa513453ce5465c8c23bacb
canisminor1990 <canisminor1990@gmail.com>
2017-03-27 22:46:16 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2377,11 +2377,9 @@ exports.default = function (room, config) {
 		extension: _.filter(structuresMy, function (structure) {
 			return structure.structureType == STRUCTURE_EXTENSION;
 		}),
-		container: _.filter(structuresOther, function (structure) {
-			return structure.structureType == STRUCTURE_CONTAINER;
-		}).sort(function (a, b) {
+		container: structuresContainer ? structuresContainer.sort(function (a, b) {
 			return b.store.energy - a.store.energy;
-		}),
+		}) : [],
 		canWithdraw: structuresDocker.length > 0 ? _.filter(structuresDocker, function (structure) {
 			return structure.store && structure.store.energy > 0;
 		}) : [],
