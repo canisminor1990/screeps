7479ef26e79c55c0d479c82bc13e7b1e54ec2261
canisminor1990 <canisminor1990@gmail.com>
2017-03-27 17:28:11 +0800

    updata

--- dist/main.js
+++ dist/main.js
@@ -3384,15 +3384,15 @@ exports.default = trigger;
 
 
 function isSafe(roomName) {
-	if (Memory.trigger[roomName].safe && Memory.rooms[roomName].creeps.enemy.length > 0) {
-		Memory.trigger[roomName] = {
+	if (Memory.trigger.noEnemy[roomName].safe && Memory.rooms[roomName].creeps.enemy.length > 0) {
+		Memory.trigger.noEnemy[roomName] = {
 			safe: false,
 			timeout: Game.time
 		};
 	}
-	if (!Memory.trigger[roomName].safe) {
-		if (Memory.trigger[roomName].timeout > 0 && Game.time - Memory.trigger[roomName].timeout > 1500) {
-			Memory.trigger[roomName].safe = true;
+	if (!Memory.trigger.noEnemy[roomName].safe) {
+		if (Memory.trigger.noEnemy[roomName].timeout > 0 && Game.time - Memory.trigger.noEnemy[roomName].timeout > 1500) {
+			Memory.trigger.noEnemy[roomName].safe = true;
 		}
 	}
 }
