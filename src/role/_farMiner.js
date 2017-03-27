17006450b50e8992962883ad1816a2ba57f9c5b7
canisminor1990 <canisminor1990@gmail.com>
2017-03-28 07:21:40 +0800

    updata

--- src/role/_farMiner.js
+++ src/role/_farMiner.js
@@ -10,6 +10,7 @@ export default (creep, roomName) => {
 	target = findInRange(harvestTarget, creep.room.memory.creeps.my.farMiner, 1, miner =>
 		miner.id != creep.id && miner.ticksToLive > 20
 	)
+	console.log(target.length)
 	if (target.length >1) targetChange(creep, Memory.rooms[roomName].sources[0].source, 'harvest')
 	if (creep.memory.full) {
 		target = findInRange(creep, creep.room.memory.structures.container, 2)[0];
