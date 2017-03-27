d8df372782c6f32d207de16ad92b0718b178c8bf
canisminor1990 <canisminor1990@gmail.com>
2017-03-27 16:50:50 +0800

    updata

--- src/manager/_memory/_sources.js
+++ src/manager/_memory/_sources.js
@@ -3,14 +3,13 @@ import { findInRange } from  '../../action'
 export default (room, miner) => {
 	const rawSources = room.find(FIND_SOURCES);
 	let sources      = []
-	rawSources.forEach(source =>
-		                   sources.push({
-			                                source: source,
-			                                miner : findInRange(source, miner, 2)
-		                                })
-	)
+	rawSources.forEach(source => sources.push({
+		                                          source: source,
+		                                          miner : findInRange(source, miner, 2)
+	                                          }))
 	if (sources.length > 0) {
-		sources.sort((a, b) => b.source.energy - a.source.energy).sort((a, b) => a.miner.length - b.miner.length)
+		sources.sort((a, b) => b.source.energy - a.source.energy)
+		       .sort((a, b) => a.miner.length - b.miner.length);
 	}
 	if (sources.length > 1 && sources[0].miner.length == 0 && sources[sources.length - 1].miner.length > 1) {
 		const targetSource = sources[sources.length - 1],
--- src/role/_miner.js
+++ src/role/_miner.js
@@ -1,5 +1,5 @@
 import { isFull, targetMaker } from '../_util'
-import { build, harvest, repair, findClosestInRange } from '../action'
+import { build, harvest, repair, findClosestInRange, moveTo } from '../action'
 export default (creep) => {
 	const memory = creep.room.memory;
 	let target;
@@ -9,7 +9,8 @@ export default (creep) => {
 	targetMaker(creep, memory.sources[0].source, 'harvest')
 	const harvestTarget = Game.getObjectById(creep.memory.target.harvest.id)
 	if (creep.memory.full) {
-		target = findClosestInRange(harvestTarget, creep.room.memory.structures.container, 1)
+		target = findClosestInRange(harvestTarget, creep.room.memory.structures.container, 2)
+		if (!creep.pos.isEqualTo(target.pos) && moveTo(creep, target)) return;
 		if (repair(creep, target, target.hits < target.hitsMax / 2)) return;
 		target = findClosestInRange(creep, creep.room.memory.structures.needBuild, 0);
 		if (build(creep, target))return;
