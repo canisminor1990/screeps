6593bc26f8e541f7407dad11362311c7711168ab
canisminor1990 <canisminor1990@gmail.com>
2017-03-28 18:58:04 +0800

    updata

--- src/config/_role2.js
+++ src/config/_role2.js
@@ -4,104 +4,33 @@ export default  (room) => {
 	const builderNumber = (needBuild.length > 0) ? needBuild.length : 1
 	const noEnemy       = Memory.trigger.noEnemy
 
-	return [
-		{
-			role    : "claim",
-			body    : {claim: 2, move: 1},
-			timeout : 100,
-			number  : (noEnemy['W81S66'].safe) ? 1 : 0,
-			priority: 7
-		},
-		{
-			role    : "farMiner",
-			body    : {work: 8, carry: 1, move: 4},
-			timeout : 100,
-			number  : (noEnemy['W81S66'].safe) ? 1 : 0,
-			priority: 1
-		},
-		{
-			role    : "farMinerSec",
-			body    : {work: 8, carry: 1, move: 4},
-			timeout : 100,
-			number  : (noEnemy['W82S67'].safe) ? 2 : 0,
-			priority: 1
-		},
-		{
-			role    : 'farHarvester',
-			body    : {carry: 8, move: 4},
-			number  : (noEnemy['W81S66'].safe) ? 3 : 0,
-			priority: 5
-		},
-		{
-			role    : 'farHarvesterSec',
-			body    : {carry: 8, move: 4},
-			number  : (noEnemy['W82S67'].safe) ? 4 : 0,
-			priority: 5
-		},
-		{
-			role    : 'farBuilder',
-			body    : {carry: 6, work: 2, move: 4},
-			number  : (noEnemy['W81S66'].safe) ? 1 : 0,
-			priority: 5
-		}, {
-			role    : 'farBuilderSec',
-			body    : {carry: 6, work: 2, move: 4},
-			number  : (noEnemy['W82S67'].safe) ? 2 : 0,
-			priority: 5
-		},
-		{
-			role    : 'harvester',
-			body    : {carry: 2, move: 1},
-			number  : 0,
-			priority: 2
-		},
-		{
-			role    : 'linker',
-			body    : {carry: 1, move: 1},
-			number  : 1,
-			priority: 2
-		},
-		{
-			role    : 'upgrader',
-			body    : {carry: 2, work: 4, move: 2},
-			number  : (builderNumber > 1) ? 1 : 3,
-			priority: 3
-		},
-		{
-			role    : 'farUpgrader',
-			body    : {carry: 5, work: 1, move: 3},
-			number  : (noEnemy['W82S67'].safe) ? 2 : 0,
-			priority: 3
-		},
-		{
-			role    : 'builder',
-			body    : {work: 2, carry: 6, move: 4},
-			number  : (builderNumber > 2) ? 2 : builderNumber,
-			priority: 6
-		},
-		{
-			role    : "miner",
-			body    : {work: 8, move: 4, carry: 1},
-			number  : 2,
-			priority: 1
-		},
-		{
-			role    : 'cleaner',
-			body    : {carry: 6, move: 3},
-			number  : 2,
-			priority: 0
-		},
-		{
-			role    : 'attacker',
-			body    : {attack: 2, move: 3},
-			number  : 0,
-			priority: 0
-		},
-		{
-			role    : 'traveller',
-			body    : {move: 1},
-			number  : 0,
-			priority: 0
-		}
-	]
+	return {
+		// [name , body , num[main,extra], time]
+		filler  : [{carry: 6}, [2, 0], 10],
+		cleaner : [{carry: 4}, [1, 0], 10],
+		miner   : [{work: 8, carry: 1}, [0, 1], 10],
+		transer : [{carry: 8}, [0, 1], 10],
+		linker  : [{carry: 1}, [0, 1], 10],
+		builder : [{work: 2, carry: 6}, [1, 1], 10],
+		repairer: [{work: 2, carry: 6}, [0, 1], 10],
+		upgrader: [{work: 4, carry: 2}, [3, 0], 10],
+		claimer : [{claim: 2}, [0, 1], 10],
+		// attacker: [{work: 8, carry: 1}, [0, 0], 100],
+	}
 }
+
+function buildBody(obj) {
+	let bodyArray = [];
+	let move      = Math.ceil(_.sum(obj) / 2);
+	_.forEach(obj, (n, key) => {
+		bodyArray = bodyArray.concat(_.fill(Array(n), key))
+	});
+	bodyArray = _.chunk(bodyArray, 2);
+	let newArray = [];
+	for (let i = 0; i < move; i++) {
+		newArray = newArray.concat(bodyArray[i],['move'])
+	};
+	return newArray
+}
+buildBody({work: 2, carry: 3})
+
