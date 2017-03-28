7ba099e2628f3019c11acfca2bde41aad2064a47
canisminor1990 <canisminor1990@gmail.com>
2017-03-28 16:57:17 +0800

    updata

--- src/task/_log.js
+++ src/task/_log.js
@@ -65,7 +65,7 @@ export default (roomName, timeout) => {
 		]
 	}
 
-	console.log(table(gclLog), table(energyLog), table(roleLog[0]), table(roleLog[1]));
+	console.log(`# ${Game.time}`, table(gclLog), table(energyLog), table(roleLog[0]), table(roleLog[1]));
 
 }
 
