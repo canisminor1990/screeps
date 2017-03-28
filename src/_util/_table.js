712ff6e82ca3864d6d7a87d974a5ad1d40e2a3c4
canisminor1990 <canisminor1990@gmail.com>
2017-03-28 17:44:20 +0800

    updata

--- src/_util/_table.js
+++ src/_util/_table.js
@@ -7,7 +7,7 @@ export default (content) => {
 	} else {
 		contentHeadr = content.header;
 		contentBody  = content.body;
-		contentHeadr.forEach(col => tableHeader += `<th style="width:72px;border:1px solid #444;padding:4px 8px">${col}</th>`)
+		contentHeadr.forEach(col => tableHeader += `<th style="width:72px;border:1px solid #444;padding:4px 8px;word-break:break-all; word-wrap:break-word;">${col}</th>`)
 		tableHeader = `<tr style="border:1px solid #444;background: #333;color:#888" >${tableHeader}</tr>`
 	}
 	contentBody.forEach(row => {
