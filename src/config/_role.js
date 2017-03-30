d22d6eda18dc8a15cc2b98ec195999b9b8a1b047
canisminor1990 <canisminor1990@gmail.com>
2017-03-30 10:45:41 +0800

    updata

--- src/config/_role.js
+++ src/config/_role.js
@@ -85,6 +85,30 @@ export default  (room) => {
 			body    : {move: 1},
 			number  : 0,
 			priority: 0
+		},
+		{
+			role    : 'cleanerSec',
+			body    : {carry: 6, move: 3},
+			number  : 2,
+			priority: 0
+		},
+		{
+			role    : 'farBuilderSec',
+			body    : {carry: 4, work: 2, move: 4},
+			number  : 2,
+			priority: 5
+		},
+		{
+			role    : 'farHarvesterSec',
+			body    : {carry: 8, move: 4},
+			number  : 5,
+			priority: 0
+		},
+		{
+			role    : 'farUpgrader',
+			body    : {carry: 6, work: 2, move: 4},
+			number  : 4,
+			priority: 3
 		}
 	]
 }
