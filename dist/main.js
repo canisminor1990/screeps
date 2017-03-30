1027f6e6eb1b4960be3dd724f444a74a15105aed
canisminor1990 <canisminor1990@gmail.com>
2017-03-30 20:15:54 +0800

    'updata'

--- dist/main.js
+++ dist/main.js
@@ -2310,27 +2310,32 @@ exports.default = function (room) {
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-exports.default = [{
-	role: 'cleanerSec',
-	body: { carry: 6, move: 3 },
-	number: 2,
-	priority: 0
-}, {
-	role: 'farBuilderSec',
-	body: { carry: 4, work: 2, move: 4 },
-	number: 2,
-	priority: 5
-}, {
-	role: 'farHarvesterSec',
-	body: { carry: 8, move: 4 },
-	number: 5,
-	priority: 0
-}, {
-	role: 'farUpgrader',
-	body: { carry: 6, work: 2, move: 4 },
-	number: 4,
-	priority: 3
-}];
+exports.default = [
+	// {
+	// 	role    : 'cleanerSec',
+	// 	body    : {carry: 6, move: 3},
+	// 	number  : 2,
+	// 	priority: 0
+	// },
+	// {
+	// 	role    : 'farBuilderSec',
+	// 	body    : {carry: 4, work: 2, move: 4},
+	// 	number  : 2,
+	// 	priority: 5
+	// },
+	// {
+	// 	role    : 'farHarvesterSec',
+	// 	body    : {carry: 8, move: 4},
+	// 	number  : 5,
+	// 	priority: 0
+	// },
+	// {
+	// 	role    : 'farUpgrader',
+	// 	body    : {carry: 6, work: 2, move: 4},
+	// 	number  : 4,
+	// 	priority: 3
+	// }
+];
 
 /***/ }),
 /* 43 */
