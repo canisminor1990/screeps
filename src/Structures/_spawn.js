835752e707eaea2a674a9d4403f8dffd4a00b8dd
canisminor1990 <canisminor1990@gmail.com>
2017-04-03 23:23:39 +0800

    update

--- src/Structures/_spawn.js
+++ src/Structures/_spawn.js
@@ -30,6 +30,9 @@ export default (spawn) => {
 		      c.memory.roomName == roleData[i].roomName &&
 		      c.memory.roomType == roleData[i].roomType &&
 		      c.ticksToLive >= roleTimeout).length;
+		Console.note(i,
+			'Now:' + roleNumberNow, 'Need:' + roleNumber,
+			'Cost:' + roleData[i].cost, 'Availabl:' + energy);
 		if (roleNumberNow - roleNumber >= 0) continue;
 		if (roleData[i].cost > energy) {
 			Console.note(i,
