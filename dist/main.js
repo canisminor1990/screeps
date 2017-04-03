d2590abb65e7b2b0703195be92750240e11cd13b
canisminor1990 <canisminor1990@gmail.com>
2017-04-04 00:01:49 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -1846,11 +1846,11 @@ var _Action = __webpack_require__(/*! ../Action */ 1);
 
 exports.default = function (creep) {
 	// target
-	if (!Game.rooms[creep.memory.roomName]) {
-		if (!Memory.rooms[creep.memory.roomName] || creep.room.name !== creep.memory.roomName) {
-			if (creep.moveTo(new RoomPosition(25, 25, creep.memory.roomName)) == OK) return;
-		}
+
+	if (!Memory.rooms[creep.memory.roomName] || creep.room.name !== creep.memory.roomName) {
+		if (creep.moveTo(new RoomPosition(25, 25, creep.memory.roomName)) == OK) return;
 	}
+
 	if ((0, _Action.reserveController)(creep, creep.room.controller)) return;
 };
 
