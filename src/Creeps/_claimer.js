44735480c264ef56e8eee08d61dd9e47bbf3a709
canisminor1990 <canisminor1990@gmail.com>
2017-04-03 23:59:31 +0800

    update

--- src/Creeps/_claimer.js
+++ src/Creeps/_claimer.js
@@ -1,10 +1,10 @@
 import {reserveController} from '../Action'
 export default (creep) => {
 	// target
-	if (!Game.rooms[creep.memory.roomName]) {
+	// if (!Game.rooms[creep.memory.roomName]) {
 		if (!Memory.rooms[creep.memory.roomName] || creep.room.name !== creep.memory.roomName) {
 			if (creep.moveTo(new RoomPosition(25, 25, creep.memory.roomName)) == OK)return
 		}
-	}
+	// }
 	if (reserveController(creep, creep.room.controller))return;
 }
