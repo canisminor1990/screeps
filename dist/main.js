ae097255b313b9096f1cacc2fa5ecb946698e717
canisminor1990 <canisminor1990@gmail.com>
2017-04-04 10:15:30 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -1900,10 +1900,6 @@ exports.default = function (creep) {
 			pickTarget = Memory.tasks[creep.memory.roomName].pickup[0];
 			if ((0, _Action.moveTo)(creep, pickTarget)) return;
 		}
-		var target = _.filter(Memory.tasks[creep.memory.roomName].withdraw, function (t) {
-			return t.store.energy == 0;
-		});
-		if (target && (0, _Action.withdraw)(creep, target)) return;
 		if ((0, _Action.pickup)(creep, pickTarget)) return;
 	}
 	if (_.sum(creep.carry) > 0 && (0, _Action.transfer)(creep, storage)) return;
