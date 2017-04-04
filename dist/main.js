03dc06a02ae3e71f23dca26a9ef7a9b821cf61c9
canisminor1990 <canisminor1990@gmail.com>
2017-04-04 08:22:47 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -3271,7 +3271,6 @@ Object.defineProperty(exports, "__esModule", {
 
 exports.default = function (terminal) {
 	if (terminal.store.energy < 10000) return;
-	console.log(Game.market.orders);
 	if (_.isEmpty(Game.market.orders)) {
 		Game.market.createOrder(ORDER_SELL, RESOURCE_ENERGY, 0.03, 8888);
 	}
