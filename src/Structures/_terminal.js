8d8a7f62c2911277eece90e5d38956ab6c285ba9
canisminor1990 <canisminor1990@gmail.com>
2017-04-04 08:22:21 +0800

    update

--- src/Structures/_terminal.js
+++ src/Structures/_terminal.js
@@ -1,5 +1,6 @@
 export default (terminal) => {
 	if (terminal.store.energy < 10000) return
+	console.log(Game.market.orders)
 	if (_.isEmpty(Game.market.orders)){
 		Game.market.createOrder(ORDER_SELL, RESOURCE_ENERGY, 0.03, 8888)
 	}
