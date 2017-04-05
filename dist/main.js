323bae7bbadf0a3e9cc0dc41e1c9d5b5325d4ea5
canisminor1990 <canisminor1990@gmail.com>
2017-04-05 10:31:53 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -422,7 +422,7 @@ exports.default = {
 		transer: [{ carry: 16 }, [1, 2], 10],
 		cleaner: [{ carry: 6 }, [1, 0], 10],
 		builder: [{ work: 2, carry: 6 }, [1, 1], 10],
-		upgrader: [{ work: 8, carry: 16 }, [2, 0], 10],
+		upgrader: [{ work: 12, carry: 2 }, [2, 0], 10],
 		claimer: [{ claim: 2 }, [0, 1], 50],
 		traveller: [{ move: 1 }, [0, 0], 10],
 		terminer: [{ carry: 16 }, [2, 0], 10]
