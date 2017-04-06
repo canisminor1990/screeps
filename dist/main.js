696d9ba99acf738e411bd3779dc4b15dd97eb3b7
canisminor1990 <canisminor1990@gmail.com>
2017-04-06 15:59:15 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -4447,12 +4447,47 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function () {
-	Memory.stats["room." + room.name + ".energyAvailable"] = room.energyAvailable;
-	Memory.stats["room." + room.name + ".energyCapacityAvailable"] = room.energyCapacityAvailable;
-	Memory.stats["room." + room.name + ".controllerProgress"] = room.controller.progress;
+	if (Memory.stats == undefined) {
+		Memory.stats = {};
+	}
+
+	var rooms = Game.rooms;
+	var spawns = Game.spawns;
+	for (var roomKey in rooms) {
+		var room = Game.rooms[roomKey];
+		var isMyRoom = room.controller ? room.controller.my : 0;
+		if (isMyRoom) {
+			Memory.stats['room.' + room.name + '.myRoom'] = 1;
+			Memory.stats['room.' + room.name + '.energyAvailable'] = room.energyAvailable;
+			Memory.stats['room.' + room.name + '.energyCapacityAvailable'] = room.energyCapacityAvailable;
+			Memory.stats['room.' + room.name + '.controllerProgress'] = room.controller.progress;
+			Memory.stats['room.' + room.name + '.controllerProgressTotal'] = room.controller.progressTotal;
+			var stored = 0;
+			var storedTotal = 0;
+
+			if (room.storage) {
+				stored = room.storage.store[RESOURCE_ENERGY];
+				storedTotal = room.storage.storeCapacity[RESOURCE_ENERGY];
+			} else {
+				stored = 0;
+				storedTotal = 0;
+			}
+
+			Memory.stats['room.' + room.name + '.storedEnergy'] = stored;
+		} else {
+			Memory.stats['room.' + room.name + '.myRoom'] = undefined;
+		}
+	}
+	Memory.stats['gcl.progress'] = Game.gcl.progress;
+	Memory.stats['gcl.progressTotal'] = Game.gcl.progressTotal;
+	Memory.stats['gcl.level'] = Game.gcl.level;
+	for (var spawnKey in spawns) {
+		var spawn = Game.spawns[spawnKey];
+		Memory.stats['spawn.' + spawn.name + '.defenderIndex'] = spawn.memory['defenderIndex'];
+	}
 };
 
-module.exports = exports["default"];
+module.exports = exports['default'];
 
 /***/ }),
 /* 87 */
