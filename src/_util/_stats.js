6fe2b4f33c5d7ec5f68a7487f93120ae668b8781
canisminor1990 <canisminor1990@gmail.com>
2017-04-07 11:26:05 +0800

    update

--- src/_util/_stats.js
+++ src/_util/_stats.js
@@ -18,16 +18,37 @@ export default (room) => {
 	// rooms
 	_.forEach(room, roomGroup => {
 		const roomName                                    = roomGroup[0],
-		      roomMain                                    = Game.rooms[roomName];
+		      roomMemory                                  = Memory.rooms[roomName];
+		// room
 		stats[`room.${roomName}.myRoom`]                  = 1;
-		stats[`room.${roomName}.energyAvailable`]         = roomMain.energyAvailable;
-		stats[`room.${roomName}.energyCapacityAvailable`] = roomMain.energyCapacityAvailable;
-		stats[`room.${roomName}.rcl`]                     = roomMain.controller.level;
-		stats[`room.${roomName}.controllerProgress`]      = roomMain.controller.progress;
-		stats[`room.${roomName}.controllerProgressTotal`] = roomMain.controller.progressTotal;
-		stats[`room.${roomName}.storedEnergy`]            = roomMain.storage.store[RESOURCE_ENERGY];
-		stats[`room.${roomName}.storedMineral`]           = _.sum(roomMain.storage.store) - roomMain.storage.store[RESOURCE_ENERGY];
-		_.forEach(Memory.rooms[roomName].creeps.my, (c = [], role) => {
+		// energy
+		stats[`room.${roomName}.energyAvailable`]         = roomMemory.energyAvailable;
+		stats[`room.${roomName}.energyCapacityAvailable`] = roomMemory.energyCapacityAvailable;
+		// rcl
+		const controller                                  = roomMemory.structures.my.controller[0];
+		stats[`room.${roomName}.rcl`]                     = controller.level;
+		stats[`room.${roomName}.controllerProgress`]      = controller.progress;
+		stats[`room.${roomName}.controllerProgressTotal`] = controller.progressTotal;
+		// storage
+		const storage                                     = roomMemory.structures.my.storage[0];
+		try {
+			stats[`room.${roomName}.storedEnergy`]  = storage.store[RESOURCE_ENERGY];
+			stats[`room.${roomName}.storedMineral`] = _.sum(storage.store) - storage.store[RESOURCE_ENERGY];
+		} catch (e) {
+			stats[`room.${roomName}.storedEnergy`]  = 0;
+			stats[`room.${roomName}.storedMineral`] = 0;
+		}
+		// terminals
+		const terminal = roomMemory.structures.my.terminal[0];
+		try {
+			stats[`room.${roomName}.terminalEnergy`]  = terminal.store[RESOURCE_ENERGY];
+			stats[`room.${roomName}.terminalMineral`] = _.sum(terminal.store) - terminal.store[RESOURCE_ENERGY];
+		} catch (e) {
+			stats[`room.${roomName}.terminalEnergy`]  = 0;
+			stats[`room.${roomName}.terminalMineral`] = 0;
+		}
+		// roles
+		_.forEach(roomMemory.creeps.my, (c = [], role) => {
 			stats[`room.${roomName}.roles.${role}`] = c.length;
 		});
 	});
