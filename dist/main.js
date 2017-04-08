a2f9b6da571517e77a9704f671bf61e228f4a31f
canisminor1990 <canisminor1990@gmail.com>
2017-04-08 11:21:50 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -2044,10 +2044,10 @@ exports.default = function (creep) {
 	var roomName = 'W81S65';
 
 	try {
-		var attackTarget = Game.getObjectById('58d17513bc53f3be7a81e407');
+		var attackTarget = Game.getObjectById('58d175038851a6135b3c2751');
 		if ((0, _Action.attack)(creep, attackTarget)) return;
 	} catch (e) {
-		if (creep.moveTo(new RoomPosition(30, 48, roomName)) == OK) return;
+		if (creep.moveTo(new RoomPosition(25, 48, roomName)) == OK) return;
 	}
 };
 
--- src/Creeps/_team.js
+++ src/Creeps/_team.js
@@ -4,9 +4,9 @@ export default (creep) => {
 	const roomName     = 'W81S65'
 	
 	try {
-		const attackTarget = Game.getObjectById('58d17513bc53f3be7a81e407')
+		const attackTarget = Game.getObjectById('58d175038851a6135b3c2751')
 		if (attack(creep, attackTarget))return;
 	} catch (e){
-		if (creep.moveTo(new RoomPosition(30, 48, roomName)) == OK)return;
+		if (creep.moveTo(new RoomPosition(25, 48, roomName)) == OK)return;
 	}
 }
