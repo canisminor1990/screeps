cca912fe653139e5747b6a21b97226130e2ddb2b
canisminor1990 <canisminor1990@gmail.com>
2017-04-08 11:35:30 +0800

    update

--- dist/main.js
+++ dist/main.js
@@ -2043,7 +2043,7 @@ exports.default = function (creep) {
 	// target
 	var roomName = 'W81S65';
 
-	(0, _Action.moveTo)(creep, Game.flags['pos']);
+	creep.moveTo(Game.flags['pos']);
 	// try {
 	// 	const attackTarget = Game.getObjectById('58d175038851a6135b3c2751')
 	// 	if (attack(creep, attackTarget))return;
