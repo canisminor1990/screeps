c26deba287832bfe6fe1ae242396b0f33bd3dd7a
canisminor1990 <canisminor1990@gmail.com>
2017-04-08 10:47:27 +0800

    update

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ export default {
 		transer  : [{carry: 16}, [1, 2], 10],
 		cleaner  : [{carry: 6}, [1, 0], 10],
 		upfiller : [{carry: 8}, [1, 0], 80],
-		upgrader : [{work: 10, carry: 1}, [1, 0], 10],
+		upgrader : [{work: 10, carry: 1}, [2, 0], 10],
 		claimer  : [{claim: 2}, [0, 1], 10],
 		builder  : [{work: 1, carry: 6}, [1, 1], 10],
 		terminer : [{carry: 16}, [1, 0], 10]
