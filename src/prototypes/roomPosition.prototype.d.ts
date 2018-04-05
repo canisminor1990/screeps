9f3201b23df6195921068f48e8c969d568d43ef4
canisminor1990 <i@canisminor.cc>
2018-04-05 17:27:08 +0800

    roomPosition.prototype

--- src/prototypes/roomPosition.prototype.d.ts
+++ src/prototypes/roomPosition.prototype.d.ts
@@ -3,8 +3,24 @@ interface LookForCache {
 	value: any[];
 }
 
+interface Pos {
+	x: number;
+	y: number;
+	roomName: string;
+}
+
 interface RoomPosition {
 	room: Room;
 	memory: RoomMemory;
+	terrain: Terrain;
+	structures: Structure[];
+	mainStructure: Structure | undefined;
+	constructionSite: ConstructionSite | undefined;
+	creep: Creep | undefined;
+	canMoveThrough: boolean;
+	isFreeSpace: boolean;
+	getAdjacentPos(range: number): RoomPosition[];
+	getFreeSpace(range: number): RoomPosition[];
+	hasStructure(type: StructureConstant): boolean;
 	cacheLookFoor(type: LookConstant, timeout?: number): any[];
 }
