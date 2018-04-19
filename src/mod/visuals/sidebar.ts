a2e40c26abb87536c1f0bbaecd11b3946d49b3ea
canisminor1990 <i@canisminor.cc>
2018-04-19 23:44:11 +0800

    update

--- src/mod/visuals/sidebar.ts
+++ src/mod/visuals/sidebar.ts
@@ -31,8 +31,8 @@ export class Sidebar extends VisualsBase {
 		this.vis = new RoomVisual(room.name);
 		if (VISUALS.ROOM_ORDERS) this.drawRoomOrders(room, 'orders');
 		if (VISUALS.ROOM_OFFERS) this.drawRoomOrders(room, 'offers');
-		if (VISUALS.STORAGE) this.drawStoreInfo(room.storage);
-		if (VISUALS.TERMINAL) this.drawStoreInfo(room.terminal);
+		if (VISUALS.STORAGE) this.drawStoreInfo(room, 'storage');
+		if (VISUALS.TERMINAL) this.drawStoreInfo(room, 'terminal');
 	};
 	private drawRoomOrders = (room: Room, type: string) => {
 		const x = 1;
@@ -52,31 +52,32 @@ export class Sidebar extends VisualsBase {
 		this.vis.text(`Room ${_.capitalize(type)}`, x, ++y, this.infoStyle);
 		this._roomOrdersObject(room.memory.resources[type], x, y + this.Y_MAEGIN - 0.5);
 	};
-	private drawStoreInfo = (type: StructureStorage | StructureTerminal): void => {
-		if (!type || !_.size(type.store)) return;
+	private drawStoreInfo = (room: Room, type: string): void => {
+		const structure: StructureStorage | StructureTerminal = room[type];
+		if (!structure || !_.size(structure.store)) return;
 		const x = 1;
 		let y = 1;
+		if (type === 'terminal' && VISUALS.STORAGE && room.storage) {
+			y += this.Y_PADDING + _.size(room.storage.store) * this.Y_LIST_MARGIN;
+		}
 		this.vis.text(`${_.capitalize(type)} Contents`, x, ++y, this.infoStyle);
-		const sum = type.sum;
-		const capacity = type.storeCapacity;
+		const sum = structure.sum;
+		const capacity = structure.storeCapacity;
 		const text1 = `L: ${Util.formatNumber(capacity - sum)}`;
 		const text2 = `${Util.formatNumber(sum)}/${Util.formatNumber(capacity)}`;
-		if (type instanceof StructureTerminal && VISUALS.STORAGE && type.room.storage) {
-			y += this.Y_PADDING + _.size(type.room.storage.store) * this.Y_LIST_MARGIN;
-		}
 		this.vis.text(`${text1} (${text2})`, x, y + 1.2, this.subStyle);
-		this._storageObject(type.store, x, y + this.Y_MAEGIN);
+		this._storageObject(structure.store, x, y + this.Y_MAEGIN);
 	};
 	private _drawList = (x, y, type, amount) => {
 		const title: string = type === RESOURCE_ENERGY ? 'E' : type;
 		const backgroundColor = this.getResourceColour(type);
-		this.vis.text(`${title}`, x, (y += this.Y_LIST_MARGIN), _.assign({ backgroundColor }, this.titleStyle));
+		this.vis.text(`${title}`, x, y, _.assign({ backgroundColor }, this.titleStyle));
 		this.vis.text(`${Util.formatNumber(amount)}`, x + 2, y, _.assign({ backgroundColor }, this.objectStyle));
 	};
 	private _roomOrdersObject = (vis, store, x, startY) => {
-		for (let order of store) this._drawList(x, startY, order.type, order.amount);
+		for (let order of store) this._drawList(x, (startY += this.Y_LIST_MARGIN), order.type, order.amount);
 	};
 	private _storageObject = (store: obj, x: number, startY: number) => {
-		Object.keys(store).forEach(type => this._drawList(x, startY, type, store[type]));
+		Object.keys(store).forEach(type => this._drawList(x, (startY += this.Y_LIST_MARGIN), type, store[type]));
 	};
 }
